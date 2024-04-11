c5ba941fabb619cdcf2ee8d867a472295bdf0ef6
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-04-12 00:18:33 +0530

    Time: 95 ms (37.17%) | Memory: 21.6 MB (26.31%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,22 +1,24 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        hashmap=defaultdict(list)
+        # Create a defaultdict of lists to hold the groups of anagrams.
+        # Each key will be a tuple representing the count of each letter,
+        # and the value will be a list of strings that are anagrams of each other.
+        hashmap = defaultdict(list)
+        
+        # Iterate over each string in the input list.
         for s in strs:
-            count=[0]*26
+            # Initialize a list of 26 zeros, one for each letter of the alphabet.
+            count = [0] * 26
+            
+            # Count the frequency of each letter in the string.
+            # 'ord(c) - ord('a')' calculates the index of the letter 'c' in the alphabet.
             for c in s:
-                count[ord(c)-ord('a')]+=1
+                count[ord(c) - ord('a')] += 1
+            
+            # Use the letter count list as a key in the hashmap.
+            # Since lists can't be used as dictionary keys (they're mutable),
+            # we convert it to a tuple (which is immutable) to use as a key.
             hashmap[tuple(count)].append(s)
-        return hashmap.values()
-    
         
-
-                
-
-
-
-
-
-
-                
-                    
-        
+        # Return the values of the hashmap, which are lists of anagram groups.
+        return list(hashmap.values())
