964879f05072bf93dc6a156ec8488b7c0c185d2d
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-04-13 17:27:14 +0530

    Time: 91 ms (47.89%) | Memory: 22.1 MB (6.22%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,22 +1,43 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        #                         index - count
+        #                         value -  list of the numbers haveing that count
+        # O(N) - Bucket approach [[]]
         
+        hashmap = {}
+        for n in nums:
+            hashmap[n]= hashmap.get(n,0) + 1
+        
+        bucket = [[] for k in range(len(nums)+1)]
+        # +1 as there mignt be zero or n
 
+        for n , c in hashmap.items():
+            bucket[c].append(n)
 
-        frequency_map = Counter(nums)
+        result =[]
+
+        for i in range(len(bucket)-1,0, -1):
+            for n in bucket[i]:
+                result.append(n)
+                if(len(result)==k):
+                    return result
+
+        # 0(k*N) -  max heap approach
+        count_map = Counter(nums)
         
         # Use a heap to store the frequency and element. Negate the frequency
         # since heapq is a min-heap, but we need a max-heap functionality.
         # The heap will store elements as (-frequency, element).
-        heap = [(-freq, num) for num, freq in frequency_map.items()]
+        heap = [(-c, n) for n, c in count_map.items()]
         
         # Transform the list into a heap in-place.
-        heapq.heapify(heap)
+        heapify(heap)
+        # heapq.heapify(heap)
         
         # Extract the top k elements (with the highest frequencies) from the heap.
         # Since the frequencies are negated, we'll use heapq.heappop which
         # will pop the smallest (in this case, the largest negative) elements first.
-        top_k = [heapq.heappop(heap)[1] for _ in range(k)]
+        top_k = [heappop(heap)[1] for _ in range(k)]
         
         return top_k
 
