d931100e404cb85fc86102bdc7e5e04beaea54cf
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-04-20 16:21:57 +0530

    Time: 52 ms (18.89%) | Memory: 19.3 MB (11.61%) - LeetSync

---
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def __init__(self):
+        self.maxi = 0
+        
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        def height(node):
+            if not node:
+                return -1 # Return -1 as the height of an empty subtree
+            
+            # Recursively get the height of the left and right subtrees
+            left_height = height(node.left)
+            right_height = height(node.right)
+            
+            # The diameter at the current node is the sum of the heights of the
+            # left and right subtrees plus 2 (for the edges connecting to the current node)
+            self.maxi = max(self.maxi, 2 + left_height + right_height)
+            
+            # Return the height of the subtree rooted at the current node
+            return 1 + max(left_height, right_height)
+        
+        height(root)  # Calculate the height of the tree and update the maximum diameter
+        return self.maxi
+        
