8ae4f37f4f98ec5f3cbc75cf0cab0a8bcdff82c9
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-04-23 11:40:06 +0530

    Time: 541 ms (47.75%) | Memory: 16.5 MB (27.33%) - LeetSync

---
+++ 668-kth-smallest-number-in-multiplication-table/kth-smallest-number-in-multiplication-table.py
@@ -0,0 +1,29 @@
+class Solution:
+    def findKthNumber(self, m: int, n: int, k: int) -> int:
+
+        
+        def issmallerthan(num) -> bool:
+            
+            count = 0
+            for div in range(1,m+1):
+                count += min( num//div , n)
+                if count ==0:
+                    break
+
+            return count >= k
+
+
+
+        l = 1
+        r = m * n
+
+        while l< r:
+            mid = (l + r)//2
+
+            if issmallerthan(mid):
+                r = mid
+            else:
+                l = mid + 1
+        
+        return l
+        
