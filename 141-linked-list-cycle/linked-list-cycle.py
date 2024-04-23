383c2d491de42485b516c8481fa430b786298442
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-04-23 22:41:51 +0530

    Time: 39 ms (89.54%) | Memory: 19.2 MB (33.11%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.py
+++ 141-linked-list-cycle/linked-list-cycle.py
@@ -1,5 +1,9 @@
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
+        if head is None:
+            return False
+        if head.next is None:
+            return False
         fast = head
         while fast and fast.next: #As fast moves 2 ptrs at a time
             head = head.next
