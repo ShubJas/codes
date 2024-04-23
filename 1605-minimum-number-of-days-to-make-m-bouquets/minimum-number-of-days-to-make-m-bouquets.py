ba2c4ff94afc512d3d5c5e29b6cc1865299c4162
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-04-23 08:51:38 +0530

    Time: 1275 ms (5.01%) | Memory: 30.5 MB (24.22%) - LeetSync

---
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.py
@@ -0,0 +1,32 @@
+class Solution:
+    def minDays(self, bloomDay: List[int], m: int, k: int) -> int:
+        
+        def feasible(days) -> bool:
+            
+            flowers = 0
+            bouquets = 0
+            for bloom in bloomDay:
+                if bloom > days:
+                    flowers = 0
+                else:
+                    bouquets+= (flowers + 1)// k 
+                    flowers=(flowers+ 1) % k
+
+            return bouquets >= m
+    
+        if len(bloomDay)< m*k:
+            return -1
+                    
+        
+        l = 1
+        r = max(bloomDay)
+
+        while(l<r):
+            #dont use same var name
+            mid = (l+r)//2
+            if(feasible(mid)):
+                r = mid
+            else:
+                l = mid +1
+
+        return l
