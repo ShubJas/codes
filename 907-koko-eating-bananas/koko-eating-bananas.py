a515f4ae00d86816c199153ca11113ad7491629c
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-04-23 08:22:45 +0530

    Time: 273 ms (47.50%) | Memory: 18.1 MB (92.06%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+
+
+        def feasible(speed) -> bool:
+            hours = 0
+            for p in piles:
+                hours+= math.ceil(p/speed)
+            if hours > h:
+                 return False
+            return True
+
+        l= 1
+        r = max(piles)
+
+        while( l < r):
+            m = (l+r)//2
+
+            if(feasible(m)):
+                r = m
+            else:
+                l = m + 1
+        return l
+        
