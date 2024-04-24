8b2bb531dd3a895430ce39bd9fafa7c76478b4b4
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-04-24 19:31:21 +0530

    Time: 374 ms (25.16%) | Memory: 53.8 MB (55.10%) - LeetSync

--- 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.py
+++ 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.py
@@ -3,7 +3,7 @@ class Solution:
         hashset = set()
         for i in range(0,len(s)-k+1):
             hashset.add(s[i:i+k])
-            if len(hashset) == math.pow(2,k):
+            if len(hashset) == 2**k:
                 return True
         return False
         
