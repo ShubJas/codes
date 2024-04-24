632edd2a26554244f643c75c061adf91e2494fa7
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-04-24 21:53:28 +0530

    Time: 80 ms (79.47%) | Memory: 17.7 MB (90.91%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -4,22 +4,22 @@ class Solution:
         hashmap2 = defaultdict(int)
         result = []
         for c in p:
-            hashmap1[c]= hashmap1.get(c,0) + 1
-        
-        for c in s[:len(p)]:
+            hashmap1[c]+= 1
+        winlength = len(p)
+        for c in s[:winlength]:
             hashmap2[c]+=1
         if hashmap1==hashmap2:
             result.append(0)
-        for i in range(len(p),len(s)):
+        for i in range(winlength,len(s)):
             hashmap2[s[i]]+=1
-            if hashmap2[s[i-len(p)]]==1:
-                del hashmap2[s[i-len(p)]]
+            if hashmap2[s[i-winlength]]==1:
+                del hashmap2[s[i-winlength]]
             else:
-                hashmap2[s[i-len(p)]]-=1
+                hashmap2[s[i-winlength]]-=1
 
             
             
             
             if hashmap1 == hashmap2:
-                result.append(i-len(p)+1)
+                result.append(i-winlength+1)
         return result
