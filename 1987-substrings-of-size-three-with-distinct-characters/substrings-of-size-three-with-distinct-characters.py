6144435f42f0cf9d884042efbe9cee0cc5b7a8ff
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-04-24 22:47:38 +0530

    Time: 39 ms (41.45%) | Memory: 16.5 MB (43.28%) - LeetSync

--- 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.py
+++ 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.py
@@ -1,20 +1,34 @@
 class Solution:
     def countGoodSubstrings(self, s: str) -> int:
-        hashmap = defaultdict(int)
+        
+        tmp = ''
         count = 0
-        for c in s[:3]:
-            hashmap[c]+=1
+
+        for i in range(len(s) - 2):
+            tmp = set(s[i:i+3])
+
+            if(len(tmp) == 3):
+                count += 1
         
-        if len(hashmap)==3:
-            count+=1
+        return count  
         
-        for i in range(3,len(s)):
-            hashmap[s[i]]+=1
-            if hashmap[s[i-3]]==1:
-                del hashmap[s[i-3]]
-            else:
-                hashmap[s[i-3]]-=1
-            if len(hashmap) == 3:
-                count+=1
-        return count
+        
+        
+        # hashmap = defaultdict(int)
+        # count = 0
+        # for c in s[:3]:
+        #     hashmap[c]+=1
+        
+        # if len(hashmap)==3:
+        #     count+=1
+        
+        # for i in range(3,len(s)):
+        #     hashmap[s[i]]+=1
+        #     if hashmap[s[i-3]]==1:
+        #         del hashmap[s[i-3]]
+        #     else:
+        #         hashmap[s[i-3]]-=1
+        #     if len(hashmap) == 3:
+        #         count+=1
+        # return count
         
