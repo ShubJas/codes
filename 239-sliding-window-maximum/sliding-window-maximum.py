d58a0f55f70f2f3c82a22e667ded001916585db4
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-04-25 08:41:48 +0530

    Time: 1092 ms (74.75%) | Memory: 33.2 MB (26.19%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.py
+++ 239-sliding-window-maximum/sliding-window-maximum.py
@@ -1,17 +1,47 @@
 from sortedcontainers import SortedList
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        
+        dq = deque()
         result = []
-        sl= SortedList()
-        for n in nums[:k]:
-            sl.add(n)
-        result.append(sl[-1])
+
+        for i in range(k):
+            while dq and nums[dq[-1]]<nums[i]:
+                dq.pop()            
+            dq.append(i)
+        result.append(nums[dq[0]])
+
         for i in range(k,len(nums)):
-            sl.remove(nums[i-k])
-            sl.add(nums[i])
-            result.append(sl[-1])
+
+            while dq and dq[0]<= i-k:
+                dq.popleft()
+
+
+            while dq and nums[dq[-1]]<nums[i]:
+                dq.pop()            
+            dq.append(i)
+            result.append(nums[dq[0]])
+
+
         
         return result
+        
+        
+        
+        
+        
+        
+        # result = []
+        # sl= SortedList()
+        # for n in nums[:k]:
+        #     sl.add(n)
+        # result.append(sl[-1])
+        # for i in range(k,len(nums)):
+        #     sl.remove(nums[i-k])
+        #     sl.add(nums[i])
+        #     result.append(sl[-1])
+        
+        # return result
 
             
 
