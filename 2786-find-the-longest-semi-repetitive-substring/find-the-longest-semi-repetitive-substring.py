fc883b7f1d6cd40428d2f97bf6c74fb81771fd15
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-04-25 10:23:14 +0530

    Time: 69 ms (53.42%) | Memory: 16.5 MB (65.41%) - LeetSync

--- 2786-find-the-longest-semi-repetitive-substring/find-the-longest-semi-repetitive-substring.py
+++ 2786-find-the-longest-semi-repetitive-substring/find-the-longest-semi-repetitive-substring.py
@@ -1,3 +1,28 @@
+# class Solution:
+#     def longestSemiRepetitiveSubstring(self, s: str) -> int:
+#         longest = 1
+#         l = 0
+
+#         while l<len(s)-1:
+#             r = l
+#             countrep = 0
+#             # Move `r` until the end of the string or until more than one repeat is found
+#             while r < len(s) - 1:
+#                 if s[r] == s[r + 1]:
+#                     countrep += 1
+#                 r += 1
+#                 if countrep > 1:
+#                     break
+
+#             # Ensure the longest calculation includes the character at position `r` if not exiting by repeat condition
+#             if countrep <= 1:
+#                 r += 1
+
+#             longest = max(longest, r - l)
+#             l = r
+
+#         return longest
+
 class Solution:
     def longestSemiRepetitiveSubstring(self, s: str) -> int:
         longest = 0
@@ -17,3 +42,4 @@ class Solution:
             longest = max(longest, r - l + 1)
 
         return longest
+
