a55d7ebdfe8079cefe40571223015446ba02241c
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-04-25 09:49:20 +0530

    Time: 98 ms (18.15%) | Memory: 16.5 MB (96.92%) - LeetSync

---
+++ 2786-find-the-longest-semi-repetitive-substring/find-the-longest-semi-repetitive-substring.py
@@ -0,0 +1,24 @@
+class Solution:
+    def longestSemiRepetitiveSubstring(self, s: str) -> int:
+        longest = 0
+        l = 0
+        if(len(s))<2:
+            return len(s)
+        for l in range(len(s)-1):
+            r = l
+            countrep = 0
+            # Move `r` until the end of the string or until more than one repeat is found
+            while r < len(s) - 1:
+                if s[r] == s[r + 1]:
+                    countrep += 1
+                r += 1
+                if countrep > 1:
+                    break
+
+            # Ensure the longest calculation includes the character at position `r` if not exiting by repeat condition
+            if countrep <= 1:
+                r += 1
+
+            longest = max(longest, r - l)
+
+        return longest
