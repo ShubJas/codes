17b2908ad4523163c83046e6df350973de49e5e0
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-05-04 09:23:53 +0530

    Time: 716 ms (53.22%) | Memory: 20.7 MB (71.12%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,28 +1,64 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
+
         result = []
-        
-        for l in range(len(nums) - 2):
-            # Skip duplicate values
-            if l > 0 and nums[l] == nums[l - 1]:
+
+        for l in range(len(nums)-2):
+
+            if l> 0 and nums[l] == nums[l-1]:
                 continue
+            
+            m= l+1
+            r = len(nums)-1
 
-            m, r = l + 1, len(nums) - 1
-            while m < r:
+            while(m<r):
                 total = nums[l] + nums[m] + nums[r]
-                if total == 0:
-                    result.append([nums[l], nums[m], nums[r]])
-                    m += 1
-                    r -= 1
-                    # Skip duplicate values
-                    while m < r and nums[m] == nums[m - 1]:
-                        m += 1
-                    while m < r and nums[r] == nums[r + 1]:
-                        r -= 1
-                elif total > 0:
-                    r -= 1
-                else:
-                    m += 1
 
+                if total == 0:
+                    result.append([nums[l],nums[m],nums[r]])
+                    m+=1
+                    r-=1
+
+                    while m<r and nums[m] == nums[m-1]:
+                        m+=1
+
+                    while m<r and nums[r] == nums[r+1]:
+                        r-=1
+                    
+
+                elif(total < 0):
+                    m+=1
+                
+                else:
+                    r-=1
         return result
+        
+        
+        
+        # nums.sort()
+        # result = []
+        
+        # for l in range(len(nums) - 2):
+        #     # Skip duplicate values
+        #     if l > 0 and nums[l] == nums[l - 1]:
+        #         continue
+
+        #     m, r = l + 1, len(nums) - 1
+        #     while m < r:
+        #         total = nums[l] + nums[m] + nums[r]
+        #         if total == 0:
+        #             result.append([nums[l], nums[m], nums[r]])
+        #             m += 1
+        #             r -= 1
+        #             # Skip duplicate values
+        #             while m < r and nums[m] == nums[m - 1]:
+        #                 m += 1
+        #             while m < r and nums[r] == nums[r + 1]:
+        #                 r -= 1
+        #         elif total > 0:
+        #             r -= 1
+        #         else:
+        #             m += 1
+
+        # return result
