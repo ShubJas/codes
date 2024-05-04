3dfeb3be79f6c16d2fd396fb0819a3b7f1ac9d76
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-05-04 16:08:12 +0530

    Time: 49 ms (17.64%) | Memory: 16.6 MB (13.55%) - LeetSync

--- 31-next-permutation/next-permutation.py
+++ 31-next-permutation/next-permutation.py
@@ -3,16 +3,44 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        i = len(nums) - 2
-        while i >= 0 and nums[i] >= nums[i + 1]:
-            i -= 1
-        
-        if i >= 0:
-            j = len(nums) - 1
-            while nums[j] <= nums[i]:
-                j -= 1
-            nums[i], nums[j] = nums[j], nums[i]
-        
-        nums[i + 1:] = reversed(nums[i + 1:])
+        piv = len(nums) -2
+        while piv>=0 and nums[piv+1]<=nums[piv]:
+            piv-=1
 
-# This code correctly modifies nums in-place to the next permutation.
+        
+        if piv>=0:
+            j= len(nums)-1
+            
+            while nums[j]<=nums[piv]:
+                j-=1
+            
+            nums[piv] , nums[j] = nums[j] , nums[piv]
+        
+
+        nums[piv+1:] = sorted(nums[piv+1:])
+        
+
+
+
+
+
+
+
+
+
+
+
+
+#         i = len(nums) - 2
+#         while i >= 0 and nums[i] >= nums[i + 1]:
+#             i -= 1
+        
+#         if i >= 0:
+#             j = len(nums) - 1
+#             while nums[j] <= nums[i]:
+#                 j -= 1
+#             nums[i], nums[j] = nums[j], nums[i]
+        
+#         nums[i + 1:] = reversed(nums[i + 1:])
+
+# # This code correctly modifies nums in-place to the next permutation.
