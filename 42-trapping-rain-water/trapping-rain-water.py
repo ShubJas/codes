5ae617452b694e96573bc5bc2ca0847752e88113
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-05-05 11:23:53 +0530

    Time: 92 ms (89.87%) | Memory: 18.4 MB (72.13%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,29 +1,35 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        if not height:
-            return 0
+        
+        l = 0
+        r = len(height)-1
+        lmax = height[l]
+        rmax = height[r]
+        total =0
 
-        n = len(height)
-        maxleft = [0] * n
-        maxright = [0] * n
-        total = 0
-
-        # Fill maxleft array
-        maxleft[0] = height[0]
-        for i in range(1, n):
-            maxleft[i] = max(maxleft[i-1], height[i])
-
-        # Fill maxright array
-        maxright[n-1] = height[n-1]
-        for i in range(n-2, -1, -1):
-            maxright[i] = max(maxright[i+1], height[i])
-
-        # Calculate total trapped water
-        for i in range(n):
-            # The trapped water at each bar is the minimum of max heights from both sides minus its own height
-            total += min(maxleft[i], maxright[i]) - height[i]
-
+        while(l<r):
+            if lmax>rmax:
+                r-=1
+                rmax = max(rmax,height[r])
+                total+= rmax - height[r]
+            else:
+                l+=1
+                lmax = max(lmax,height[l])
+                total+= lmax - height[l]
+        
         return total
+
+
+
+
+
+
+
+
+
+
+
+
 
         # total = 0
 
