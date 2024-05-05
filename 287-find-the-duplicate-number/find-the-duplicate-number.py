9f5ea79fb4458d38dfbf1002c1539eea848b8879
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-05-05 11:39:56 +0530

    Time: 460 ms (71.13%) | Memory: 38.5 MB (6.87%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/find-the-duplicate-number.py
@@ -1,8 +1,23 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        ctr = Counter(nums)
+        
+        hashset = {}
 
-        for value,count in ctr.items():
-            if count>1:
-                return value 
+        for val in nums:
+            hashset[val] = hashset.get(val,0) + 1
+        
+        for val ,  count in hashset.items():
+            if count >1:
+                return val
+        
+        
+        
+        
+        
+        
+        # ctr = Counter(nums)
+
+        # for value,count in ctr.items():
+        #     if count>1:
+        #         return value 
         
