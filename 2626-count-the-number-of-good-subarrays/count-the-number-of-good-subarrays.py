b99ba54ba4411490492a4c2d07b80fb01a144b50
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-05-07 08:54:57 +0530

    Time: 498 ms (75.50%) | Memory: 34.5 MB (22.00%) - LeetSync

--- 2626-count-the-number-of-good-subarrays/count-the-number-of-good-subarrays.py
+++ 2626-count-the-number-of-good-subarrays/count-the-number-of-good-subarrays.py
@@ -1,5 +1,29 @@
 class Solution:
     def countGood(self, nums: List[int], k: int) -> int:
+        
+        # l = 0
+
+        # pairs = result = count =  0
+        # hashset = defaultdict(int)
+        # for r in range(len(nums)):
+            
+        #     count += hashset[nums[r]]
+        #     hashset[nums[r]] +=1
+
+        #     if count>=k:
+        #         result = len(nums) - r
+        #         remove = nums[l]
+        #         hashset[remove] -=1
+        #         count-= hashset[remove]
+        #         l+=1
+        
+        # return result
+            
+        
+
+
+
+
 
         left = ans = tally = 0
         n, d = len(nums), defaultdict(int)
