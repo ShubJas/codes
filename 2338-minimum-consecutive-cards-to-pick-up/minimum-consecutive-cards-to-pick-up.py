ec3fab429bbc604186ca516a13f266144cecb3a4
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-05-07 09:50:01 +0530

    Time: 605 ms (76.03%) | Memory: 36.4 MB (22.55%) - LeetSync

--- 2338-minimum-consecutive-cards-to-pick-up/minimum-consecutive-cards-to-pick-up.py
+++ 2338-minimum-consecutive-cards-to-pick-up/minimum-consecutive-cards-to-pick-up.py
@@ -1,22 +1,37 @@
 class Solution:
     def minimumCardPickup(self, cards: List[int]) -> int:
-        min_count = len(cards) + 1
-        hashset = defaultdict(int)
-        l = 0
+        
+        value_index = {}
+        min_count  = -1
+        for i in range(len(cards)):
+            card = cards[i]
+            if card in value_index:
+                if min_count == -1:
+                    min_count = i - value_index[card] + 1
+                else:
+                    min_count = min(min_count ,  i - value_index[card] + 1)
+            value_index[card] = i
+        return min_count
+        
+        
+        
+        # min_count = len(cards) + 1
+        # hashset = defaultdict(int)
+        # l = 0
 
-        for r in range(len(cards)):
+        # for r in range(len(cards)):
 
-            current = cards[r]
+        #     current = cards[r]
 
-            hashset[current] +=1
+        #     hashset[current] +=1
 
-            while hashset[current]>1:
+        #     while hashset[current]>1:
                 
-                min_count = min(min_count,r-l +1)
-                remove = cards[l]
-                hashset[remove] -=1
-                l+=1
-        return min_count if min_count != len(cards) + 1 else -1
+        #         min_count = min(min_count,r-l +1)
+        #         remove = cards[l]
+        #         hashset[remove] -=1
+        #         l+=1
+        # return min_count if min_count != len(cards) + 1 else -1
 
 
         
