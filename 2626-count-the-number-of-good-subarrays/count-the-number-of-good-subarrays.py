ca361c34a969f6373fea0a051b9c2197b662ed61
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-05-07 08:57:42 +0530

    Time: 487 ms (88.50%) | Memory: 34.3 MB (94.50%) - LeetSync

--- 2626-count-the-number-of-good-subarrays/count-the-number-of-good-subarrays.py
+++ 2626-count-the-number-of-good-subarrays/count-the-number-of-good-subarrays.py
@@ -1,23 +1,23 @@
 class Solution:
     def countGood(self, nums: List[int], k: int) -> int:
         
-        # l = 0
+        l = 0
 
-        # pairs = result = count =  0
-        # hashset = defaultdict(int)
-        # for r in range(len(nums)):
+        pairs = result = count =  0
+        hashset = defaultdict(int)
+        for r in range(len(nums)):
             
-        #     count += hashset[nums[r]]
-        #     hashset[nums[r]] +=1
+            count += hashset[nums[r]]
+            hashset[nums[r]] +=1
 
-        #     if count>=k:
-        #         result = len(nums) - r
-        #         remove = nums[l]
-        #         hashset[remove] -=1
-        #         count-= hashset[remove]
-        #         l+=1
+            while count>=k:
+                result += len(nums) - r
+                remove = nums[l]
+                hashset[remove] -=1
+                count-= hashset[remove]
+                l+=1
         
-        # return result
+        return result
             
         
 
@@ -25,17 +25,17 @@ class Solution:
 
 
 
-        left = ans = tally = 0
-        n, d = len(nums), defaultdict(int)
+        # left = ans = tally = 0
+        # n, d = len(nums), defaultdict(int)
 
-        for right,num in enumerate(nums):    # <-- 1
-            tally += d[num]
-            d[num] += 1
+        # for right,num in enumerate(nums):    # <-- 1
+        #     tally += d[num]
+        #     d[num] += 1
             
-            while tally >= k:                # <-- 2     
-                ans+= n - right
-                d[nums[left]] -= 1           # <-- 3
-                tally -= d[nums[left]]
-                left += 1
+        #     while tally >= k:                # <-- 2     
+        #         ans+= n - right
+        #         d[nums[left]] -= 1           # <-- 3
+        #         tally -= d[nums[left]]
+        #         left += 1
             
-        return ans                           # <-- 4
+        # return ans                           # <-- 4
