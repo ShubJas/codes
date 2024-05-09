a3fd233c48906b66bb3fab26f188693593d0efb6
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-05-09 05:59:31 +0530

    Time: 845 ms (51.03%) | Memory: 30.6 MB (55.74%) - LeetSync

--- 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.py
+++ 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.py
@@ -1,36 +1,57 @@
 class Solution:
     def minOperations(self, nums: List[int], x: int) -> int:
-        
-        
+
+        l =0
         n = len(nums)
-        prefix = [0] * (n+1)
-        rev_suffix = [0] * (n+1)
-        min_op = float("inf")
-        # start with one buffer zero
+        max_window = -1
+        target = sum(nums) - x
+        window = 0
+        for r in range(n):
+            window += nums[r]
+            
+            while l <= r and window >target:
+                window -= nums[l]
+                l+=1 
 
-        for i in range(1,n+1):
-            prefix[i] = prefix[i-1] + nums[i-1]
+            if window == target:
+                max_window = max(max_window, r - l + 1)
+            
+            
 
-            if prefix[i] == x:
-                min_op = min(min_op,i)
-        
-        for i in range(n-1,-1,-1):
-            rev_suffix[n-i] = rev_suffix[n-i-1] + nums[i]
+        return -1 if max_window== -1 else n - max_window 
 
-            if rev_suffix == x:
-                min_op = min(min_op,n-i)
-
-        
-        prefix_map = {prefix[i]: i for i in range(n+1)}
-
-        for i in range(n+1):
-            remaining = x - rev_suffix[i]
-
-            if remaining in prefix_map and prefix_map[remaining] + i <=n:
-                min_op = min(min_op,i + prefix_map[remaining])
 
         
-        return min_op if min_op != float("inf") else -1
+        
+        # n = len(nums)
+        # prefix = [0] * (n+1)
+        # rev_suffix = [0] * (n+1)
+        # min_op = float("inf")
+        # # start with one buffer zero
+
+        # for i in range(1,n+1):
+        #     prefix[i] = prefix[i-1] + nums[i-1]
+
+        #     if prefix[i] == x:
+        #         min_op = min(min_op,i)
+        
+        # for i in range(n-1,-1,-1):
+        #     rev_suffix[n-i] = rev_suffix[n-i-1] + nums[i]
+
+        #     if rev_suffix == x:
+        #         min_op = min(min_op,n-i)
+
+        
+        # prefix_map = {prefix[i]: i for i in range(n+1)}
+
+        # for i in range(n+1):
+        #     remaining = x - rev_suffix[i]
+
+        #     if remaining in prefix_map and prefix_map[remaining] + i <=n:
+        #         min_op = min(min_op,i + prefix_map[remaining])
+
+        
+        # return min_op if min_op != float("inf") else -1
 
         
         
