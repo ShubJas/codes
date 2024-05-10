5071a3f4ddbb729bb64cbe5c15dc419efa1724a9
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-05-10 08:27:14 +0530

    Time: 217 ms (93.93%) | Memory: 21.5 MB (17.38%) - LeetSync

---
+++ 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.py
@@ -0,0 +1,12 @@
+from collections import Counter
+from itertools import accumulate
+
+class Solution:
+    def subarraysDivByK(self, nums: List[int], k: int) -> int:
+        remainders = Counter(map(lambda x: x % k, accumulate(nums)))        
+        ans = 0
+        for r in remainders:
+            # combinations of prefixes have same remainer mod k
+            ans += remainders[r] * (remainders[r]-1) // 2
+        ans += remainders[0]
+        return ans
