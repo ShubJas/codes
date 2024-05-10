279e5e3a89938b5b548692210a8ec9c956947e6a
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-05-10 07:28:21 +0530

    Time: 819 ms (46.83%) | Memory: 43.5 MB (34.74%) - LeetSync

--- 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.py
+++ 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.py
@@ -1,15 +1,27 @@
 class Solution:
     def maximumHappinessSum(self, happiness: List[int], k: int) -> int:
 
-        total = 0
-        hashset = defaultdict(list)
-        for i in range(len(happiness)):
-            hashset[happiness[i]].append(i)
-        happiness.sort()
-        for i in range(k):
-            curr =happiness[len(happiness)-1-i]
-            total += curr - i if curr - i > 0 else 0
-            del hashset[curr][0]
         
+        total = 0
+        happiness.sort(reverse=True)
+        for i in range(k):
+
+            total += happiness[i] - i if happiness[i] - i > 0 else 0
+
         return total
+        
+        
+        
+        
+        # total = 0
+        # hashset = defaultdict(list)
+        # for i in range(len(happiness)):
+        #     hashset[happiness[i]].append(i)
+        # happiness.sort()
+        # for i in range(k):
+        #     curr =happiness[len(happiness)-1-i]
+        #     total += curr - i if curr - i > 0 else 0
+        #     del hashset[curr][0]
+        
+        # return total
         
