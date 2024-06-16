0d95d4684977d5c7b1cad670fa2bf6d883b0be7d
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-06-16 18:15:06 +0530

    Time: 41 ms (80.67%) | Memory: 17.3 MB (99.07%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.py
+++ 110-balanced-binary-tree/balanced-binary-tree.py
@@ -6,24 +6,56 @@
 #         self.right = right
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        if not root:
-            return True
+
+        return self.depth(root) >=0
 
 
-        return self.depth(root)[0]
     
+    def depth(self,root):
 
-    def depth(self, root):
         if not root:
-            return [True,0]
+            return 0
         
-        left = self.depth(root.left) 
-        right = self.depth (root.right) 
+        left = self.depth(root.left)
+        right = self.depth(root.right)
+
+        if left < 0 or right < 0 or abs(left-right) > 1:
+            return -1
+        
+        return 1 + max(left,right)
 
 
-        balanced = left[0] and right[0] and abs(left[1]- right[1]) < 2
 
-        return [balanced,1 + max(left[1],right[1])]
+# Definition for a binary tree node.
+# # class TreeNode:
+# #     def __init__(self, val=0, left=None, right=None):
+# #         self.val = val
+# #         self.left = left
+# #         self.right = right
+# class Solution:
+#     def isBalanced(self, root: Optional[TreeNode]) -> bool:
+
+#         if not root:
+#             return True
+        
+
+#         return self.depth(root)[0]
+
 
     
+
+#     def depth(self,node):
+#         if not node:
+#             return [True,0]
+        
+#         left = self.depth(node.left)
+#         right =  self.depth(node.right)
+
+#         balanced = left[0] and right[0] and abs(left[1]-right[1])<2 
+
+#         return [balanced,1 + max(left[1],right[1])]
+        
+
+
+        
         
