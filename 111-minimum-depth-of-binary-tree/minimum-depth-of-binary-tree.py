ba28d0d8a807f4f0783bc312ecdb259f7f70fdd9
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-06-17 14:29:32 +0530

    Time: 238 ms (68.61%) | Memory: 43.2 MB (72.08%) - LeetSync

--- 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.py
+++ 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.py
@@ -4,25 +4,66 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+
+# BFS
 class Solution:
     def minDepth(self, root: Optional[TreeNode]) -> int:
 
+        q = collections.deque([(root,1)])
 
-        if not root:
-            return 0
+
+        while q:
+
+            curr, curr_depth = q.popleft()
+
+
+            if not curr:
+                continue
+            
+            if not curr.right and not curr.left:
+                return curr_depth
+            
+
+            q.append([curr.left,curr_depth + 1])
+            q.append([curr.right,curr_depth + 1])
         
 
-        left_depth = self.minDepth(root.left)
-        right_depth = self.minDepth(root.right)
+        return 0
 
-        if not root.left and not root.right:
-            return 1
-        
-        if not root.left:
-            return 1 + right_depth
-        
-        if not root.right:
-            return 1 + left_depth
+
+
+
+
+
+# # Definition for a binary tree node.
+# # class TreeNode:
+# #     def __init__(self, val=0, left=None, right=None):
+# #         self.val = val
+# #         self.left = left
+# #         self.right = right
+
+# # DFS
+# class Solution:
+#     def minDepth(self, root: Optional[TreeNode]) -> int:
+
+
+#         if not root:
+#             return 0
         
 
-        return 1 + min(left_depth,right_depth)
+#         left_depth = self.minDepth(root.left)
+#         right_depth = self.minDepth(root.right)
+
+#         if not root.left and not root.right:
+#             return 1
+        
+#         if not root.left:
+#             return 1 + right_depth
+        
+#         if not root.right:
+#             return 1 + left_depth
+        
+
+#         return 1 + min(left_depth,right_depth)
+        
