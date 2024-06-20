449141556685094ea8959d96fe95c9dc695d073b
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-06-20 07:00:38 +0530

    Time: 32 ms (84.02%) | Memory: 16.5 MB (46.54%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.py
+++ 101-symmetric-tree/symmetric-tree.py
@@ -4,17 +4,60 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+# BFS
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
 
-        return self.same(root.left,root.right)
 
-    def same(self,p,q):
+        q = collections.deque([(root,root)])
 
-        if not p and not q: 
-            return True
-        if (not p or not q) or (p.val != q.val):
-            return False
+        while q:
+            a , b = q.popleft()
+
+            if not a and not b:
+                continue
+            
+            if not a or not b:
+                return False
+            
+            if a.val != b.val:
+                return False
+            
+            q.append((a.left,b.right))
+            q.append((a.right,b.left))
         
-        return self.same(p.left,q.right) and self.same(p.right,q.left)
+        return True
+        
+
+
+        
+
+
+
+
+
+
+
+
+#  DFS
+# # Definition for a binary tree node.
+# # class TreeNode:
+# #     def __init__(self, val=0, left=None, right=None):
+# #         self.val = val
+# #         self.left = left
+# #         self.right = right
+# class Solution:
+#     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+
+#         return self.same(root.left,root.right)
+
+#     def same(self,p,q):
+
+#         if not p and not q: 
+#             return True
+#         if (not p or not q) or (p.val != q.val):
+#             return False
+        
+#         return self.same(p.left,q.right) and self.same(p.right,q.left)
         
