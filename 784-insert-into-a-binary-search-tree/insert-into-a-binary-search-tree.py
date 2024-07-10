07272d4228b6e8ff76b3cbff87e29c366766fe5b
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-07-10 21:56:30 +0530

    Time: 86 ms (33.04%) | Memory: 18.7 MB (44.83%) - LeetSync

--- 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.py
+++ 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.py
@@ -1,23 +1,24 @@
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 class Solution:
     def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
-
+        # If the tree is empty, create a new node with the given value and return it
         if not root:
-            root = TreeNode(val)
-            return root
+            return TreeNode(val)
 
+        # If the value to be inserted is greater than the root's value,
+        # insert it into the right subtree
         if val > root.val:
-            root.right = self.insertIntoBST(root.right,val)
-
+            root.right = self.insertIntoBST(root.right, val)
+        # If the value to be inserted is less than the root's value,
+        # insert it into the left subtree
         elif val < root.val:
-            root.left = self.insertIntoBST(root.left,val)
+            root.left = self.insertIntoBST(root.left, val)
         
+        # Return the root node after insertion
         return root
-
-
-        
