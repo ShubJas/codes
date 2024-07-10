80752e8f709e8818c1ab71caa780fbdcb5f2db56
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-07-10 21:26:38 +0530

    Time: 65 ms (8.31%) | Memory: 19.8 MB (77.56%) - LeetSync

--- 450-delete-node-in-a-bst/delete-node-in-a-bst.py
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.py
@@ -1,35 +1,43 @@
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 class Solution:
     def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
-        
+        # If the tree is empty, return None
         if not root:
             return root
-
-        if root.val < key:
-            root.right = self.deleteNode(root.right,key)
         
-        elif root.val > key:
-            root.left = self.deleteNode(root.left,key)
-
+        # If the key to be deleted is smaller than the root's key,
+        # then it lies in the left subtree
+        if root.val > key:
+            root.left = self.deleteNode(root.left, key)
+        # If the key to be deleted is greater than the root's key,
+        # then it lies in the right subtree
+        elif root.val < key:
+            root.right = self.deleteNode(root.right, key)
+        # If the key is the same as the root's key, then this is the node to be deleted
         else:
-            if not root.left:
-                root = root.right
-            elif not root.right:
-                root = root.left
-            else:
-
-                curr = root.right
-
-                while curr.left:
-                    curr = curr.left
-                root.val = curr.val
-
-                root.right = self.deleteNode(root.right,root.val)
-
-
+            # Node with only one child or no child
+            if not root.right:
+                return root.left
+            elif not root.left:
+                return root.right
+            
+            # Node with two children: Get the inorder successor (smallest
+            # in the right subtree)
+            curr = root.right
+            while curr.left:
+                curr = curr.left
+            
+            # Copy the inorder successor's value to this node
+            root.val = curr.val
+            
+            # Delete the inorder successor
+            root.right = self.deleteNode(root.right, root.val)
+        
+        # Return the root node after deletion
         return root
