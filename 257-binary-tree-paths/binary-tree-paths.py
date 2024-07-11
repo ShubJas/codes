3b84049863fdfb9d76e13203ee20c96495d728cf
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-07-11 13:06:38 +0530

    Time: 37 ms (53.65%) | Memory: 16.6 MB (6.41%) - LeetSync

--- 257-binary-tree-paths/binary-tree-paths.py
+++ 257-binary-tree-paths/binary-tree-paths.py
@@ -4,60 +4,27 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
 class Solution:
     def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
-        result = []  # List to store all root-to-leaf paths
 
-        def dfs(root, path, result):
+        result = []
+
+
+
+        def dfs(root,path, result):
+
+
             if not root:
-                return  # If the current node is None, return immediately
+                return 
             
-            path.append(str(root.val))  # Add the current node's value to the path list
+            path += str(root.val)
 
             if not root.left and not root.right:
-                # If the current node is a leaf, append the path to the result list
-                result.append('->'.join(path))
+                result.append(path)
             else:
-                # If the current node is not a leaf, continue the depth-first search
-                dfs(root.left, path, result)  # Traverse the left subtree
-                dfs(root.right, path, result)  # Traverse the right subtree
-            
-            path.pop()  # Backtrack by removing the last element from the path list
+                dfs(root.left,path+'->',result)
+                dfs(root.right,path+'->',result)
         
-        dfs(root, [], result)  # Initialize the DFS with an empty path list
-        return result  # Return the list of root-to-leaf paths
 
-
-
-
-
-
-
-# # Definition for a binary tree node.
-# # class TreeNode:
-# #     def __init__(self, val=0, left=None, right=None):
-# #         self.val = val
-# #         self.left = left
-# #         self.right = right
-
-# class Solution:
-#     def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
-#         result = []  # List to store all root-to-leaf paths
-
-#         def dfs(root, path, result):
-#             if not root:
-#                 return  # If the current node is None, return immediately
-            
-#             path += str(root.val)  # Add the current node's value to the path
-            
-#             if not root.left and not root.right:
-#                 # If the current node is a leaf, append the path to the result list
-#                 result.append(path)
-#             else:
-#                 # If the current node is not a leaf, continue the depth-first search
-#                 dfs(root.left, path + '->', result)  # Traverse the left subtree
-#                 dfs(root.right, path + '->', result)  # Traverse the right subtree
-        
-#         dfs(root, '', result)  # Initialize the DFS with an empty path
-#         return result  # Return the list of root-to-leaf paths
+        dfs(root,'',result)
+        return result
