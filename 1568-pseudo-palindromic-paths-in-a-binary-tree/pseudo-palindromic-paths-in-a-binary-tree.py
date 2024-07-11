409cc8e90ed0481ba294857481a57917abbaf7f9
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-07-11 22:42:14 +0530

    Time: 404 ms (46.46%) | Memory: 43.4 MB (53.20%) - LeetSync

--- 1568-pseudo-palindromic-paths-in-a-binary-tree/pseudo-palindromic-paths-in-a-binary-tree.py
+++ 1568-pseudo-palindromic-paths-in-a-binary-tree/pseudo-palindromic-paths-in-a-binary-tree.py
@@ -7,30 +7,32 @@
 class Solution:
     def pseudoPalindromicPaths (self, root: Optional[TreeNode]) -> int:
 
-        freq = defaultdict(int)
         self.result = 0
+        freq = defaultdict(int)
 
         def dfs(root,odd):
 
             if not root:
-                return
-            freq[root.val] +=1
-            curr_odd = 1 if freq[root.val] % 2 == 1 else -1
-            odd += curr_odd
-            if not root.left and not root.right:
-                if odd <= 1:
-                    self.result+=1
+                return 
             
+            freq[root.val] +=1
+            curr_odd = 1 if freq[root.val] & 1 else -1
+            odd += curr_odd
+
+            if not root.left and not root.right:
+                if odd <2:
+                    self.result +=1
+                
             dfs(root.left,odd)
             dfs(root.right,odd)
             freq[root.val] -=1
             odd -= curr_odd
         
         dfs(root,0)
-
         return self.result
-                
-            
 
-            
+
+
+
+
         
