37a5e1df97ea7028644d995de49ff85fc488b507
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-07-11 12:01:02 +0530

    Time: 35 ms (70.11%) | Memory: 16.7 MB (6.41%) - LeetSync

--- 257-binary-tree-paths/binary-tree-paths.py
+++ 257-binary-tree-paths/binary-tree-paths.py
@@ -4,30 +4,30 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 class Solution:
     def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
-        
-        result = []
+        result = []  # List to store all root-to-leaf paths
 
-
-        def dfs(root,path,result):
-
-
+        def dfs(root, path, result):
             if not root:
-                return
+                return  # If the current node is None, return immediately
             
-            path.append(str(root.val))
+            path.append(str(root.val))  # Add the current node's value to the path list
 
             if not root.left and not root.right:
+                # If the current node is a leaf, append the path to the result list
                 result.append('->'.join(path))
             else:
-                dfs(root.left,path,result)
-                dfs(root.right,path,result)
-            path.pop()
+                # If the current node is not a leaf, continue the depth-first search
+                dfs(root.left, path, result)  # Traverse the left subtree
+                dfs(root.right, path, result)  # Traverse the right subtree
+            
+            path.pop()  # Backtrack by removing the last element from the path list
         
-        dfs(root,[],result)
+        dfs(root, [], result)  # Initialize the DFS with an empty path list
+        return result  # Return the list of root-to-leaf paths
 
-        return result
 
 
 
