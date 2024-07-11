a9e32116e206422cbbc4d79a9a7145470a8e18ad
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-07-11 13:34:06 +0530

    Time: 43 ms (43.54%) | Memory: 17.5 MB (21.36%) - LeetSync

--- 112-path-sum/path-sum.py
+++ 112-path-sum/path-sum.py
@@ -4,24 +4,22 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
-
-
-        def inorder_dfs(root,curr_sum):
-
+        # Inner function to perform DFS and check path sums
+        def inorder_dfs(root, curr_sum):
             if not root:
-                return False
+                return False  # If the current node is None, there is no path, return False
             
-            curr_sum += root.val
+            curr_sum += root.val  # Add the current node's value to the current sum
 
+            # If it's a leaf node, check if the current sum equals the targetSum
             if not root.left and not root.right:
                 return curr_sum == targetSum
             
-            return inorder_dfs(root.left,curr_sum)  or inorder_dfs(root.right, curr_sum)
+            # Recursively check the left and right subtrees
+            return inorder_dfs(root.left, curr_sum) or inorder_dfs(root.right, curr_sum)
         
-
-        return inorder_dfs(root,0)
-
-
-        
+        # Start the DFS from the root with an initial sum of 0
+        return inorder_dfs(root, 0)
