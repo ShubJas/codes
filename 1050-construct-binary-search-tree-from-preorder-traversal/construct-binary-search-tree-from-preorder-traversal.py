66f294860a4210f2a396cab4222f1507f7823b6d
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-07-12 15:29:34 +0530

    Time: 34 ms (87.01%) | Memory: 16.5 MB (83.54%) - LeetSync

--- 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.py
+++ 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.py
@@ -4,24 +4,27 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 class Solution:
     def bstFromPreorder(self, preorder: List[int]) -> Optional[TreeNode]:
-
+        # Base case: if preorder list is empty, return None
         if not preorder:
-            return
+            return None
 
+        # The first element in preorder is the root of the BST
         root = TreeNode(preorder[0])
+        
+        # If there is only one element, return it as the root
         if len(preorder) == 1:
             return root
 
+        # Find the boundary between left and right subtrees
         i = 1
         while i < len(preorder) and preorder[i] < preorder[0]:
-            i+=1
+            i += 1
 
-
+        # Recursively construct the left and right subtrees
         root.left = self.bstFromPreorder(preorder[1:i])
-        root.right = self.bstFromPreorder(preorder[i:]) 
-
+        root.right = self.bstFromPreorder(preorder[i:])
+        
         return root
-        
-        
