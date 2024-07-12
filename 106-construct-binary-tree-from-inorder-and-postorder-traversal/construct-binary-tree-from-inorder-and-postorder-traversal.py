ae9402ebe147cb8514b25a8dcc1a6106d1929f4c
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-07-12 14:07:10 +0530

    Time: 147 ms (13.09%) | Memory: 88.3 MB (18.12%) - LeetSync

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.py
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.py
@@ -4,16 +4,26 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+#  write in NB n Visulize
 class Solution:
     def buildTree(self, inorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
-
+        # Base case: if either list is empty, return None (no tree/subtree)
         if not inorder or not postorder:
             return
         
+        # The last element in postorder is always the root of the current subtree
         root = TreeNode(postorder[-1])
-        mid = inorder.index(postorder[-1])
-        root.left = self.buildTree(inorder[:mid],postorder[:mid])
-        root.right = self.buildTree(inorder[mid+1:],postorder[mid:-1])
-
-        return root
         
+        # Find the index of the root in the inorder list
+        mid = inorder.index(postorder[-1])
+        
+        # Recursively build the left subtree
+        # Left subtree: inorder[:mid], postorder[:mid]
+        root.left = self.buildTree(inorder[:mid], postorder[:mid])
+        
+        # Recursively build the right subtree
+        # Right subtree: inorder[mid+1:], postorder[mid:-1]
+        root.right = self.buildTree(inorder[mid+1:], postorder[mid:-1])
+        
+        # Return the constructed tree/subtree
+        return root
