0b8dab4785d36afa307920638135fbebab0930bf
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-07-14 16:51:46 +0530

    Time: 189 ms (33.07%) | Memory: 17.3 MB (70.72%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.py
+++ 547-number-of-provinces/number-of-provinces.py
@@ -10,10 +10,12 @@ class Solution:
         root_y = self.find(y)
         if root_x != root_y:
             self.parent[root_y] = root_x
+            self.count +=1
 
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
         n = len(isConnected)
         self.parent = list(range(n))  # Initialize parent list
+        self.count = 0
 
         for i in range(n):
             for j in range(i + 1, n):
@@ -21,6 +23,7 @@ class Solution:
                     self.union(i, j)
 
         # Find all unique roots to determine the number of provinces
-        unique_roots = set(self.find(i) for i in range(n))
-        return len(unique_roots)
+        # unique_roots = set(self.find(i) for i in range(n))
+        return n - self.count
+        # return len(unique_roots)
 
