e7f7e1b7ced8be7f92dfa3dbe59bad203069c56a
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-07-14 16:47:53 +0530

    Time: 181 ms (69.77%) | Memory: 17.2 MB (89.34%) - LeetSync

---
+++ 547-number-of-provinces/number-of-provinces.py
@@ -0,0 +1,26 @@
+class Solution:
+    def find(self, x):
+        # Path compression heuristic
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self, x, y):
+        root_x = self.find(x)
+        root_y = self.find(y)
+        if root_x != root_y:
+            self.parent[root_y] = root_x
+
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        n = len(isConnected)
+        self.parent = list(range(n))  # Initialize parent list
+
+        for i in range(n):
+            for j in range(i + 1, n):
+                if isConnected[i][j] == 1:
+                    self.union(i, j)
+
+        # Find all unique roots to determine the number of provinces
+        unique_roots = set(self.find(i) for i in range(n))
+        return len(unique_roots)
+
