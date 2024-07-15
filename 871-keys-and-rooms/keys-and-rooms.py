5bd3fec7b6de00ea84f740ad15d144a5d2fea795
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-07-16 01:02:54 +0530

    Time: 74 ms (12.69%) | Memory: 16.9 MB (96.59%) - LeetSync

--- 871-keys-and-rooms/keys-and-rooms.py
+++ 871-keys-and-rooms/keys-and-rooms.py
@@ -1,32 +1,53 @@
 class Solution:
-    def dfs(self, room, rooms):
-        # If this room has already been visited, return
-        if self.visited[room]:
-            return
-        # Mark this room as visited
-        self.visited[room] = True
-        # Get the keys available in the current room
-        can_unlock = rooms[room]
-
-        # Recursively visit each room that can be unlocked with the keys available
-        for i in can_unlock:
-            self.dfs(i, rooms)
-
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
-        # Number of rooms
-        n = len(rooms)
-        # Initialize the visited list to keep track of visited rooms
-        self.visited = [False] * n
-        
-        # Start DFS from room 0
-        self.dfs(0, rooms)
+        visited = [False] * len(rooms)
+        stack = [0]
 
-        # Check if all rooms have been visited
-        # for x in self.visited:
-        #     if not x:
-        #         return False
+        while stack:
+            curr = stack.pop()
+            if not visited[curr]:
+                visited[curr] = True
+                for key in rooms[curr]:
+                    if not visited[key]:
+                        stack.append(key)
         
-        # If all rooms are visited, return True
-        # return True
+        return all(visited)
+
+
+
+
+
+# class Solution:
+#     def dfs(self, room, rooms):
+#         # If this room has already been visited, return
+#         if self.visited[room]:
+#             return
+#         # Mark this room as visited
+#         self.visited[room] = True
+#         # Get the keys available in the current room
+#         can_unlock = rooms[room]
+
+#         # Recursively visit each room that can be unlocked with the keys available
+#         for i in can_unlock:
+#             self.dfs(i, rooms)
+
+#     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+#         # Number of rooms
+#         n = len(rooms)
+#         # Initialize the visited list to keep track of visited rooms
+#         self.visited = [False] * n
         
-        return all(self.visited)
+#         # Start DFS from room 0
+#         self.dfs(0, rooms)
+
+#         # Check if all rooms have been visited
+#         # for x in self.visited:
+#         #     if not x:
+#         #         return False
+        
+#         # If all rooms are visited, return True
+#         # return True
+        
+#         return all(self.visited)
+
+        
