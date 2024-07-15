091ab26806ae85fc2d917779f0bfe574d3e5f602
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-07-15 23:13:40 +0530

    Time: 994 ms (38.32%) | Memory: 41.2 MB (85.38%) - LeetSync

--- 1492-time-needed-to-inform-all-employees/time-needed-to-inform-all-employees.py
+++ 1492-time-needed-to-inform-all-employees/time-needed-to-inform-all-employees.py
@@ -1,19 +1,19 @@
 class Solution:
-    def dfs(self, idx, master_maps, informTime, memo):
+    def dfs(self, idx, manager_emp, informTime, memo):
         # If the inform time for this employee is already calculated, return it
         if memo[idx] != -1:
             return memo[idx]
         
         # If the current employee has no subordinates (leaf node)
-        if informTime[idx] == 0:
-            memo[idx] = 0  # Base case: no time needed to inform subordinates
+        if idx not in manager_emp:
+            # memo[idx] = 0  # Base case: no time needed to inform subordinates
             return 0
         
         max_time = 0
         # Check all subordinates of the current manager (idx)
-        for i in master_maps[idx]:
+        for i in manager_emp[idx]:
             # Recursively call dfs for each subordinate and update max_time
-            max_time = max(max_time, self.dfs(i, master_maps, informTime, memo))
+            max_time = max(max_time, self.dfs(i, manager_emp, informTime, memo))
         
         # Calculate the total inform time for this employee and store it in memo
         memo[idx] = max_time + informTime[idx]
@@ -22,9 +22,9 @@ class Solution:
     def numOfMinutes(self, n: int, headID: int, manager: List[int], informTime: List[int]) -> int:
         # Initialize memoization list with -1 (indicating uncalculated inform times)
         memo = [-1] * n
-        master_maps = defaultdict(list) # boss: employees
+        manager_emp = defaultdict(list) # boss: employees
         for i, m in enumerate(manager):
             if m!= -1:
-                master_maps[m].append(i)
+                manager_emp[m].append(i)
         # Calculate the total inform time starting from the headID
-        return self.dfs(headID, master_maps, informTime, memo)
+        return self.dfs(headID, manager_emp, informTime, memo)
