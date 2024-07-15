ff9fb855bf0bd1d1ec0a4e0548aa1abd5ae2f97a
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-07-16 00:48:50 +0530

    Time: 65 ms (58.46%) | Memory: 17 MB (74.82%) - LeetSync

---
+++ 871-keys-and-rooms/keys-and-rooms.py
@@ -0,0 +1,25 @@
+class Solution:
+    def dfs(self,room,rooms):
+        if self.visited[room]:
+            return
+        self.visited[room] = True
+        can_unlock = rooms[room]
+
+        for i in can_unlock:
+            self.dfs(i,rooms)
+
+    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+
+        n = len(rooms)
+        self.visited = [False] * n  
+        
+
+        self.dfs(0,rooms)
+
+        for x in self.visited:
+            if not x:
+                return False
+        
+        return True
+
+        
