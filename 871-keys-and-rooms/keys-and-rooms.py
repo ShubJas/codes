3d64fcc013450bd4a49c21d638ae2b21d88bdcc5
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-07-16 00:58:47 +0530

    Time: 57 ms (93.27%) | Memory: 17 MB (36.98%) - LeetSync

--- 871-keys-and-rooms/keys-and-rooms.py
+++ 871-keys-and-rooms/keys-and-rooms.py
@@ -1,25 +1,32 @@
 class Solution:
-    def dfs(self,room,rooms):
+    def dfs(self, room, rooms):
+        # If this room has already been visited, return
         if self.visited[room]:
             return
+        # Mark this room as visited
         self.visited[room] = True
+        # Get the keys available in the current room
         can_unlock = rooms[room]
 
+        # Recursively visit each room that can be unlocked with the keys available
         for i in can_unlock:
-            self.dfs(i,rooms)
+            self.dfs(i, rooms)
 
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
-
+        # Number of rooms
         n = len(rooms)
-        self.visited = [False] * n  
+        # Initialize the visited list to keep track of visited rooms
+        self.visited = [False] * n
         
+        # Start DFS from room 0
+        self.dfs(0, rooms)
 
-        self.dfs(0,rooms)
-
+        # Check if all rooms have been visited
         # for x in self.visited:
         #     if not x:
         #         return False
         
-        return all(self.visited)
-
+        # If all rooms are visited, return True
+        # return True
         
+        return all(self.visited)
