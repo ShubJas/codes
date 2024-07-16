c943956fc37a5e3db543c71a47912da44b555160
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-07-16 21:24:58 +0530

    Time: 115 ms (53.63%) | Memory: 17.8 MB (31.42%) - LeetSync

--- 690-employee-importance/employee-importance.py
+++ 690-employee-importance/employee-importance.py
@@ -1,65 +1,72 @@
-"""
 # Definition for Employee.
 class Employee:
     def __init__(self, id: int, importance: int, subordinates: List[int]):
         self.id = id
         self.importance = importance
         self.subordinates = subordinates
-"""
 
 class Solution:
     def getImportance(self, employees: List['Employee'], id: int) -> int:
-        self.total =0
+        self.total = 0  # Initialize total importance
+        
+        # Dictionaries to store employee importance and subordinates
         emp_sub = defaultdict(list)
-        emp_imp = defaultdict(list)
+        emp_imp = defaultdict(int)
+        
+        # Populate the dictionaries with employee data
         for emp in employees:
             idx = emp.id
             emp_imp[idx] = emp.importance
             emp_sub[idx] = emp.subordinates
-
-        stack = [(id,emp_imp[id])]
+        
+        # Initialize a stack with the starting employee id and its importance
+        stack = [(id, emp_imp[id])]
+        
+        # Perform DFS using the stack
         while stack:
-            idx , imp = stack.pop() 
-            self.total += imp
+            idx, imp = stack.pop()  # Pop an employee id and its importance from the stack
+            self.total += imp  # Add the importance to the total
+            
+            # If the employee has subordinates, add them to the stack
             if len(emp_sub[idx]) != 0:
                 for emp in emp_sub[idx]:
-                    stack.append((emp,emp_imp[emp]))
-
-                
-        return self.total
-
+                    stack.append((emp, emp_imp[emp]))
+        
+        return self.total  # Return the total importance
 
 
-# """
 # # Definition for Employee.
 # class Employee:
 #     def __init__(self, id: int, importance: int, subordinates: List[int]):
 #         self.id = id
 #         self.importance = importance
 #         self.subordinates = subordinates
-# """
 
 # class Solution:
-#     def dfs(self,idx,emp_imp,emp_sub):
-
-#         self.total +=emp_imp[idx]
-        
-#         if len(emp_sub[idx]) == 0:
-#             return 
-        
-#         for emp in emp_sub[idx]:
-#             self.dfs(emp,emp_imp,emp_sub)
-
 #     def getImportance(self, employees: List['Employee'], id: int) -> int:
-#         self.total =0
+#         self.total = 0  # Initialize total importance
+        
+#         # Dictionaries to store employee importance and subordinates
 #         emp_sub = defaultdict(list)
-#         emp_imp = defaultdict(list)
+#         emp_imp = defaultdict(int)
+        
+#         # Populate the dictionaries with employee data
 #         for emp in employees:
 #             idx = emp.id
 #             emp_imp[idx] = emp.importance
 #             emp_sub[idx] = emp.subordinates
-
-#         self.dfs(id,emp_imp,emp_sub)
-                
-#         return self.total
-
+        
+#         # Initialize a stack with the starting employee id and its importance
+#         stack = [(id, emp_imp[id])]
+        
+#         # Perform DFS using the stack
+#         while stack:
+#             idx, imp = stack.pop()  # Pop an employee id and its importance from the stack
+#             self.total += imp  # Add the importance to the total
+            
+#             # If the employee has subordinates, add them to the stack
+#             if len(emp_sub[idx]) != 0:
+#                 for emp in emp_sub[idx]:
+#                     stack.append((emp, emp_imp[emp]))
+        
+#         return self.total  # Return the total importance
