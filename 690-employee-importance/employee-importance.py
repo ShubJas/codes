c4447ca6aa1990d5ac155e7bbfc60b23e8e9a3c8
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-07-16 21:22:43 +0530

    Time: 101 ms (98.87%) | Memory: 17.8 MB (31.42%) - LeetSync

--- 690-employee-importance/employee-importance.py
+++ 690-employee-importance/employee-importance.py
@@ -8,16 +8,6 @@ class Employee:
 """
 
 class Solution:
-    def dfs(self,idx,emp_imp,emp_sub):
-
-        self.total +=emp_imp[idx]
-        
-        if len(emp_sub[idx]) == 0:
-            return 
-        
-        for emp in emp_sub[idx]:
-            self.dfs(emp,emp_imp,emp_sub)
-
     def getImportance(self, employees: List['Employee'], id: int) -> int:
         self.total =0
         emp_sub = defaultdict(list)
@@ -27,6 +17,49 @@ class Solution:
             emp_imp[idx] = emp.importance
             emp_sub[idx] = emp.subordinates
 
-        self.dfs(id,emp_imp,emp_sub)
+        stack = [(id,emp_imp[id])]
+        while stack:
+            idx , imp = stack.pop() 
+            self.total += imp
+            if len(emp_sub[idx]) != 0:
+                for emp in emp_sub[idx]:
+                    stack.append((emp,emp_imp[emp]))
+
                 
         return self.total
+
+
+
+# """
+# # Definition for Employee.
+# class Employee:
+#     def __init__(self, id: int, importance: int, subordinates: List[int]):
+#         self.id = id
+#         self.importance = importance
+#         self.subordinates = subordinates
+# """
+
+# class Solution:
+#     def dfs(self,idx,emp_imp,emp_sub):
+
+#         self.total +=emp_imp[idx]
+        
+#         if len(emp_sub[idx]) == 0:
+#             return 
+        
+#         for emp in emp_sub[idx]:
+#             self.dfs(emp,emp_imp,emp_sub)
+
+#     def getImportance(self, employees: List['Employee'], id: int) -> int:
+#         self.total =0
+#         emp_sub = defaultdict(list)
+#         emp_imp = defaultdict(list)
+#         for emp in employees:
+#             idx = emp.id
+#             emp_imp[idx] = emp.importance
+#             emp_sub[idx] = emp.subordinates
+
+#         self.dfs(id,emp_imp,emp_sub)
+                
+#         return self.total
+
