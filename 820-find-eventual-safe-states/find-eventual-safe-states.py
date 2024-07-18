006f43586ce8fbd79871817c2644c809befeaf1f
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-07-19 01:22:00 +0530

    Time: 508 ms (84.88%) | Memory: 24 MB (83.87%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.py
+++ 820-find-eventual-safe-states/find-eventual-safe-states.py
@@ -1,33 +1,33 @@
-# Cycle detection using DFS and dp
+# Cycle detection using DFS and self.visited
 class Solution:
-    def dfs(self, i, graph, dp):
+    def dfs(self, i, graph):
         # If the node has been processed before, return its state (True if safe, False otherwise)
-        if dp[i] != 0:
-            return dp[i] == 1
+        if self.visited[i] != 0:
+            return self.visited[i] == 1
 
         # Mark the node as being processed to detect cycles
-        dp[i] = -1
+        self.visited[i] = -1
 
         # Recursively process all the neighbors
         for con in graph[i]:
             # If any neighbor is not safe, the current node is also not safe
-            if not self.dfs(con, graph, dp):
+            if not self.dfs(con, graph):
                 return False
         
         # If all neighbors are safe, mark the current node as safe
-        dp[i] = 1
+        self.visited[i] = 1
         return True
 
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
         
         result = []  # List to store the safe nodes
         n = len(graph)  # Number of nodes in the graph
-        dp = [0] * n  # dp array to store the state of each node (-1: being processed, 1: safe, 0: unknown)
+        self.visited = [0] * n  # self.visited array to store the state of each node (-1: being processed, 1: safe, 0: unknown)
 
         # Process each node in the graph
         for i in range(n):
             # If the node is safe, add it to the result list
-            if self.dfs(i, graph, dp):
+            if self.dfs(i, graph):
                 result.append(i)
 
         return result  # Return the list of safe nodes
