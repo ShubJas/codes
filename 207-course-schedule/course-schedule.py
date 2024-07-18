2dfac794f9a5ba2ba71166868c2c2008d2d84fd1
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-07-19 01:03:53 +0530

    Time: 84 ms (84.87%) | Memory: 18.4 MB (23.67%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,28 +1,65 @@
+# DFS
 class Solution:
+    def dfs(self,i,graph):
+
+        if self.visited[i] != 0:
+            return self.visited[i] == 1
+        
+        self.visited[i] = -1
+
+        for neighbor in graph[i]:
+            if not self.dfs(neighbor,graph):
+                return False
+        
+        self.visited[i] = 1
+        return True
+
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         # Initialize in-degree array and adjacency list
-        indeg = [0] * numCourses
+        self.visited = [0] * numCourses
         graph = defaultdict(list)
 
-        # Build the graph and update in-degrees
+        # Build the graph 
         for course, pre in prerequisites:
             graph[pre].append(course)
-            indeg[course] += 1
 
-        # Initialize the queue with all nodes having in-degree 0
-        q = deque([i for i in range(numCourses) if indeg[i] == 0])
-        topo = []
 
-        # Process the nodes with in-degree 0
-        while q:
-            course = q.popleft()
-            topo.append(course)
+        for i in range(numCourses):
+            if self.visited[i] == 0:
+                if not self.dfs(i,graph):
+                    return False
 
-            # Update the in-degrees of neighbors and add to queue if in-degree becomes 0
-            for neighbor in graph[course]:
-                indeg[neighbor] -= 1
-                if indeg[neighbor] == 0:
-                    q.append(neighbor)
+        return True
 
-        # If topological sort includes all courses, return True, else return False
-        return len(topo) == numCourses
+        
+       
+
+# # KHAN/ BFS
+# class Solution:
+#     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+#         # Initialize in-degree array and adjacency list
+#         indeg = [0] * numCourses
+#         graph = defaultdict(list)
+
+#         # Build the graph and update in-degrees
+#         for course, pre in prerequisites:
+#             graph[pre].append(course)
+#             indeg[course] += 1
+
+#         # Initialize the queue with all nodes having in-degree 0
+#         q = deque([i for i in range(numCourses) if indeg[i] == 0])
+#         topo = []
+
+#         # Process the nodes with in-degree 0
+#         while q:
+#             course = q.popleft()
+#             topo.append(course)
+
+#             # Update the in-degrees of neighbors and add to queue if in-degree becomes 0
+#             for neighbor in graph[course]:
+#                 indeg[neighbor] -= 1
+#                 if indeg[neighbor] == 0:
+#                     q.append(neighbor)
+
+#         # If topological sort includes all courses, return True, else return False
+#         return len(topo) == numCourses
