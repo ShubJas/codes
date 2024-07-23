70274c0a0d3402950107eae7a88eb572a523ca4b
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-07-23 16:56:45 +0530

    Time: 42 ms (16.62%) | Memory: 16.5 MB (22.63%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,37 +1,23 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
+
+
         n = len(nums)
-        # Base case: if there's no house, the maximum amount is 0.
-        if n == 0:
+
+        if n==0:
             return 0
-        # Base case: if there's only one house, rob that house.
         elif n == 1:
             return nums[0]
-        # Base case: if there are two houses, rob the one with more money.
-        elif n == 2:
-            return max(nums)
-        
 
-        dp =[-1] * (n+1)
+        dp = [-1] * (n + 1)
+        dp[0] = nums[0]
+        dp[1] = max(nums[0],nums[1])
 
-        def calc(i):
-
-            if dp[i] != -1:
-                return dp[i]
-
-            if i == 0:
-                return nums[0]
+        for i in range(2,n):
+            pick = nums[i] + dp[i-2]
+            not_pick = dp[i-1]
             
-            if i == 1:
-                return max(nums[0],nums[1])
-
-        
-            pick = nums[i] + calc(i-2)
-            not_pick = calc(i-1)
-            
-
             dp[i] = max(pick,not_pick)
-            return dp[i]
         
-        return calc(n-1)
-
+        return dp[n-1]
+        
