0a3b8e33a35245b0155a450e43d8e1c80835398e
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-07-23 12:11:15 +0530

    Time: 30 ms (89.62%) | Memory: 16.5 MB (10.90%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,3 +1,4 @@
+# Bottom - up - iterative ( optimized)
 class Solution:
     def fib(self, n: int) -> int:
         # Handle the base case where n is 0
@@ -20,3 +21,56 @@ class Solution:
         
         # Return the Fibonacci number for index n
         return p1
+        
+# # Bottom - up - iterative 
+# class Solution:
+#     def fib(self, n: int) -> int:
+#         # Initialize a list to store the computed Fibonacci values. 
+#         # The size of the list is n+1 because we want to include the value for index n.
+#         dp = [-1] * (n + 1)
+        
+#         # Base cases: 
+#         # fib(0) is 0
+#         dp[0] = 0
+        
+#         # fib(1) is 1. 
+#         # We need to handle the case when n is 0 separately to avoid index out of range error.
+#         if n > 0:
+#             dp[1] = 1
+
+#         # Fill the dp array from index 2 to n.
+#         for i in range(2, n + 1):
+#             # fib(i) is the sum of fib(i-1) and fib(i-2)
+#             dp[i] = dp[i - 1] + dp[i - 2]
+        
+#         # Return the Fibonacci number for index n.
+#         return dp[n]
+
+
+
+# # TOP down -recusion
+# class Solution:
+#     def fib(self, n: int) -> int:
+#         # Initialize a list to store the computed Fibonacci values. 
+#         # The size of the list is n+1 because we want to include the value for index n.
+#         dp = [-1] * (n + 1)
+        
+#         def fibbo(i):
+#             # Base case: if i is 0 or 1, return i. 
+#             # This is because fib(0) = 0 and fib(1) = 1.
+#             if i <= 1:
+#                 return i
+            
+#             # If the value for fib(i) is already computed and stored in dp, return that value.
+#             if dp[i] != -1:
+#                 return dp[i]
+            
+#             # Otherwise, compute the value by making recursive calls to fibbo(i-1) and fibbo(i-2)
+#             # and store the result in dp[i].
+#             dp[i] = fibbo(i - 1) + fibbo(i - 2)
+            
+#             # Return the computed value for fib(i).
+#             return dp[i]
+        
+#         # Start the computation by calling fibbo(n). This will compute and return fib(n).
+#         return fibbo(n)
