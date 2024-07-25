4db5bf84b3c7039261969d9cd3668ff9bd376e67
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-07-25 18:40:29 +0530

    Time: 1496 ms (28.34%) | Memory: 32.1 MB (45.60%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -1,5 +1,3 @@
-# Intuition -  S1 + S2 = S , S1 = S2 , SO S1 = S2 = S/2
-# find any subset = S/2
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
 
@@ -11,42 +9,79 @@ class Solution:
         reach =  S//2
 
         dp = [[None] * (reach+1) for _ in range(n)]
+
+
+        for i in range(n):
+            dp[i][0] = True
         
-        def calc(i,target):
+        if nums[0] <= reach:
+            dp[0][nums[0]] = True
 
 
-            if target == 0:
-                return True
-            
-            if i == 0:
-                return target == nums[0]
-            
-            if dp[i][target] is not None:
-                return dp[i][target]
-            
-            pick = False
-            if nums[i] <= target:
-                pick = calc(i-1,target-nums[i])
-            
-            not_pick  = calc(i-1,target)
+        for i in range(1,n):
 
-            dp[i][target] = pick or not_pick
-            return dp[i][target]
+            for target in range(1,reach+1):
+
+                pick = False
+                if nums[i] <= target:
+                    pick = dp[i-1][target-nums[i]]
+                
+                not_pick  = dp[i-1][target]
+
+                dp[i][target] = pick or not_pick
+
+        return dp[n-1][reach]
+
         
-        return calc(n-1,reach)
 
+# class Solution:
+#     def canPartition(self, nums: List[int]) -> bool:
+
+#         S = sum(nums)
+#         if S % 2 != 0:
+#             return False
+
+#         n = len(nums)
+#         reach =  S//2
+
+#         dp = [[None] * (reach+1) for _ in range(n)]
+        
+#         def calc(i,target):
+
+
+#             if target == 0:
+#                 return True
+            
+#             if i == 0:
+#                 return target == nums[0]
+            
+#             if dp[i][target] is not None:
+#                 return dp[i][target]
+            
+#             pick = False
+#             if nums[i] <= target:
+#                 pick = calc(i-1,target-nums[i])
+            
+#             not_pick  = calc(i-1,target)
+
+#             dp[i][target] = pick or not_pick
+#             return dp[i][target]
+        
+#         return calc(n-1,reach)
 
 # # Intuition -  S1 + S2 = S , S1 = S2 , SO S1 = S2 = S/2
 # # find any subset = S/2
 # class Solution:
 #     def canPartition(self, nums: List[int]) -> bool:
 
-#         reach = sum(nums)/2
-
-#         if int(reach) != reach:
+#         S = sum(nums)
+#         if S % 2 != 0:
 #             return False
 
-        
+#         n = len(nums)
+#         reach =  S//2
+
+
 #         def calc(i,target):
 
 
@@ -65,4 +100,4 @@ class Solution:
 
 #             return pick or not_pick
         
-#         return calc(len(nums)-1,reach)
+#         return calc(n-1,reach)
