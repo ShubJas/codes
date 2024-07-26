2c72f689c8c78c3ab25e53c48334597de45823bd
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-07-27 00:34:05 +0530

    Time: 69 ms (92.62%) | Memory: 16.4 MB (98.69%) - LeetSync

--- 494-target-sum/target-sum.py
+++ 494-target-sum/target-sum.py
@@ -19,30 +19,79 @@ class Solution:
         reach  = (S- target) // 2
         
 
-        dp = [[0] * (reach+1) for _ in range(n)]
+        prev = [0] * (reach+1)
 
 
-        for i in range(n):
-            dp[i][0] = 1
+        prev[0] = 1
 
         if nums[0] <= reach:
-            dp[0][nums[0]] = 1
+            prev[nums[0]] = 1
 
         for i in range(1,n):
-
+            curr = [0] * (reach+1)
+            curr[0] = 1
             for target in range(1,reach+1):
 
                 pick = 0
                 if nums[i] <=target:
-                    pick = dp[i-1][target-nums[i]]
+                    pick = prev[target-nums[i]]
                 
-                npick = dp[i-1][target]
+                npick = prev[target]
 
-                dp[i][target] = pick + npick
+                curr[target] = pick + npick
+            prev = curr
         
-        count_nonzeros = dp[n-1][reach]
+        count_nonzeros = prev[reach]
 
         return count_nonzeros * pow(2,zero_count)
+
+
+
+# class Solution:
+#     def findTargetSumWays(self, nums: List[int], target: int) -> int:
+
+#         zero_count = nums.count(0)
+
+#         if zero_count == len(nums):
+
+#             if target == 0:
+#                 return pow(2,zero_count)
+#             else:
+#                 return 0
+
+#         nums = [ele for ele in nums if ele != 0]
+#         n = len(nums)
+#         S = sum(nums) 
+
+#         if (S - target) & 1 or S - target < 0:
+#             return 0
+#         reach  = (S- target) // 2
+        
+
+#         dp = [[0] * (reach+1) for _ in range(n)]
+
+
+#         for i in range(n):
+#             dp[i][0] = 1
+
+#         if nums[0] <= reach:
+#             dp[0][nums[0]] = 1
+
+#         for i in range(1,n):
+
+#             for target in range(1,reach+1):
+
+#                 pick = 0
+#                 if nums[i] <=target:
+#                     pick = dp[i-1][target-nums[i]]
+                
+#                 npick = dp[i-1][target]
+
+#                 dp[i][target] = pick + npick
+        
+#         count_nonzeros = dp[n-1][reach]
+
+#         return count_nonzeros * pow(2,zero_count)
 
 # class Solution:
 #     def findTargetSumWays(self, nums: List[int], target: int) -> int:
