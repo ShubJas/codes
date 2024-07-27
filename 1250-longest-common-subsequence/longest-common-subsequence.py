4db770b65ac1985af74974d383ce7c6d645a35f8
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-07-27 18:45:21 +0530

    Time: 747 ms (23.70%) | Memory: 16.6 MB (95.23%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -4,36 +4,71 @@ class Solution:
         n1 = len(text1)
         n2 = len(text2)
 
-        dp = [[0] * n2 for _ in range(n1)]
+        prev = [0] * n2
 
         for i2 in range(n2):
             if text1[0] in list(text2[:i2+1]):
-                dp[0][i2] = 1
+                prev[i2] = 1
         
-        for i1 in range(n1):
-            if text2[0] in list(text1[:i1+1]):
-                dp[i1][0] = 1
+        # for i1 in range(n1):
+        #     if text2[0] in list(text1[:i1+1]):
+        #         dp[i1][0] = 1
 
         for i1 in range(1,n1):
-
+            curr = [0] * n2
+            if text2[0] in list(text1[:i1+1]):
+                curr[0] = 1
             for i2 in range(1,n2):
 
 
                 take = -float('inf') 
 
                 if text1[i1] == text2[i2]:
-                    take = 1 + dp[i1-1][i2-1]
+                    take = 1 + prev[i2-1]
 
-                ntake1 = dp[i1-1][i2]
-                ntake2 = dp[i1][i2-1]
-
-
-                dp[i1][i2] = max(take,ntake1,ntake2)
+                ntake1 = prev[i2]
+                ntake2 = curr[i2-1]
 
 
+                curr[i2] = max(take,ntake1,ntake2)
+            prev = curr     
+
+        return prev[n2-1]
+
+
+# class Solution:
+#     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+
+#         n1 = len(text1)
+#         n2 = len(text2)
+
+#         dp = [[0] * n2 for _ in range(n1)]
+
+#         for i2 in range(n2):
+#             if text1[0] in list(text2[:i2+1]):
+#                 dp[0][i2] = 1
         
+#         for i1 in range(n1):
+#             if text2[0] in list(text1[:i1+1]):
+#                 dp[i1][0] = 1
 
-        return dp[n1-1][n2-1]
+#         for i1 in range(1,n1):
+
+#             for i2 in range(1,n2):
+
+
+#                 take = -float('inf') 
+
+#                 if text1[i1] == text2[i2]:
+#                     take = 1 + dp[i1-1][i2-1]
+
+#                 ntake1 = dp[i1-1][i2]
+#                 ntake2 = dp[i1][i2-1]
+
+
+#                 dp[i1][i2] = max(take,ntake1,ntake2)     
+
+#         return dp[n1-1][n2-1]
 
 # class Solution:
 #     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
