0e8f66e92fe4d9b79492c47838b5f34a73ce35ae
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-07-27 12:06:24 +0530

    Time: 233 ms (49.74%) | Memory: 37.4 MB (33.61%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.py
+++ 518-coin-change-ii/coin-change-ii.py
@@ -4,39 +4,75 @@ class Solution:
         n = len(coins)
 
 
-        dp = [[-1] * (amount + 1) for _ in range(n)]
-        def calc(i,target):
+        dp = [[0] * (amount + 1) for _ in range(n)]
+
+
+        for i in range(n):
+
+            dp[i][0] = 1
+        
+
+        for target in range(amount+1):
+            if target % coins[0] == 0:
+                dp[0][target] = 1
+            else:
+                dp[0][target] = 0
+        
+
+        for i in range(1,n):
+
+            for target in range(1,amount+1):
+
+                take =0
+                if coins[i] <= target:
+                    take = dp[i][target - coins[i]]
+                
+                ntake = dp[i-1][target]
+
+                dp[i][target] = take + ntake
+
+    
+        return dp[n-1][amount]
 
 
 
-            if target == 0:
-                return 1
+# class Solution:
+#     def change(self, amount: int, coins: List[int]) -> int:
+
+#         n = len(coins)
+
+
+#         dp = [[-1] * (amount + 1) for _ in range(n)]
+#         def calc(i,target):
+
+
+
+#             if target == 0:
+#                 return 1
             
 
-            if i == 0:
-                if target % coins[0] == 0:
-                    return 1
-                else:
-                    return 0
+#             if i == 0:
+#                 if target % coins[0] == 0:
+#                     return 1
+#                 else:
+#                     return 0
 
-            if dp[i][target] != -1:
-                return dp[i][target]
+#             if dp[i][target] != -1:
+#                 return dp[i][target]
 
-            take =0
-            if coins[i] <= target:
-                take = calc(i,target - coins[i])
+#             take =0
+#             if coins[i] <= target:
+#                 take = calc(i,target - coins[i])
             
-            ntake = calc(i-1,target)
+#             ntake = calc(i-1,target)
 
-            dp[i][target] = take + ntake
+#             dp[i][target] = take + ntake
             
 
-            return dp[i][target]
+#             return dp[i][target]
 
         
-        return calc(n-1,amount)
-
-
+#         return calc(n-1,amount)
 
 # class Solution:
 #     def change(self, amount: int, coins: List[int]) -> int:
