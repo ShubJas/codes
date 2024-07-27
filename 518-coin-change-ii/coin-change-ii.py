a0a39f77e72eaebb0c1ee162db2db0dbbc28c27d
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-07-27 12:08:24 +0530

    Time: 190 ms (57.29%) | Memory: 16.8 MB (68.40%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.py
+++ 518-coin-change-ii/coin-change-ii.py
@@ -4,36 +4,73 @@ class Solution:
         n = len(coins)
 
 
-        dp = [[0] * (amount + 1) for _ in range(n)]
+        prev = [0] * (amount + 1)
 
 
-        for i in range(n):
-
-            dp[i][0] = 1
+        prev[0] = 1
         
 
         for target in range(amount+1):
             if target % coins[0] == 0:
-                dp[0][target] = 1
+                prev[target] = 1
             else:
-                dp[0][target] = 0
+                prev[target] = 0
         
 
         for i in range(1,n):
-
+            curr = [0] * (amount + 1)
+            curr[0] = 1
+            
             for target in range(1,amount+1):
 
                 take =0
                 if coins[i] <= target:
-                    take = dp[i][target - coins[i]]
+                    take = curr[target - coins[i]]
                 
-                ntake = dp[i-1][target]
+                ntake = prev[target]
 
-                dp[i][target] = take + ntake
+                curr[target] = take + ntake
+            prev = curr
 
     
-        return dp[n-1][amount]
+        return prev[amount]
 
+
+# class Solution:
+#     def change(self, amount: int, coins: List[int]) -> int:
+
+#         n = len(coins)
+
+
+#         dp = [[0] * (amount + 1) for _ in range(n)]
+
+
+#         for i in range(n):
+
+#             dp[i][0] = 1
+        
+
+#         for target in range(amount+1):
+#             if target % coins[0] == 0:
+#                 dp[0][target] = 1
+#             else:
+#                 dp[0][target] = 0
+        
+
+#         for i in range(1,n):
+
+#             for target in range(1,amount+1):
+
+#                 take =0
+#                 if coins[i] <= target:
+#                     take = dp[i][target - coins[i]]
+                
+#                 ntake = dp[i-1][target]
+
+#                 dp[i][target] = take + ntake
+
+    
+#         return dp[n-1][amount]
 
 
 # class Solution:
