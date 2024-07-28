58d79dabcc92661101643aedb8dd4365e6ff1d54
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-07-28 14:40:18 +0530

    Time: 1429 ms (32.86%) | Memory: 41.6 MB (45.84%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.py
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.py
@@ -1,34 +1,56 @@
+#  s1 = s , s2 = rev(s) , now lcs in them is longest pal
+
 class Solution:
     def longestPalindromeSubseq(self, s: str) -> int:
 
         n = len(s)
+        t = s[::-1]
 
-        dp = [[-1] * (n) for _ in range(n)]
+        # Initialize dp array
+        dp = [[0] * (n + 1) for _ in range(n + 1)]
 
-        def calc(i1,i2):
-
-            
-            if i1 > i2:  # if the starting index crosses the ending index
-                return 0
-
-
-            if i1 == i2:
-                return 1
-            
-            if dp[i1][i2] != -1:
-                return dp[i1][i2]
-            
-            # p = -float('inf')
-            if s[i1] == s[i2]:
-                # pick case
-                dp[i1][i2] = 2 + calc(i1+1,i2-1)
-            else:
-                # not pick case
-                dp[i1][i2] = max(calc(i1+1,i2),calc(i1,i2-1))
-            return dp[i1][i2]
+        # Fill the dp array
+        for i1 in range(1, n + 1):
+            for i2 in range(1, n + 1):
+                if s[i1 - 1] == t[i2 - 1]:
+                    dp[i1][i2] = 1 + dp[i1 - 1][i2 - 1]
+                else:
+                    dp[i1][i2] = max(dp[i1 - 1][i2], dp[i1][i2 - 1])
         
-        return calc(0,n-1)
         
+        return dp[n][n]
+
+
+
+# class Solution:
+#     def longestPalindromeSubseq(self, s: str) -> int:
+
+#         n = len(s)
+
+#         dp = [[-1] * (n) for _ in range(n)]
+
+#         def calc(i1,i2):
+
+            
+#             if i1 > i2:  # if the starting index crosses the ending index
+#                 return 0
+
+
+#             if i1 == i2:
+#                 return 1
+            
+#             if dp[i1][i2] != -1:
+#                 return dp[i1][i2]
+            
+#             if s[i1] == s[i2]:
+#                 # pick case
+#                 dp[i1][i2] = 2 + calc(i1+1,i2-1)
+#             else:
+#                 # not pick case
+#                 dp[i1][i2] = max(calc(i1+1,i2),calc(i1,i2-1))
+#             return dp[i1][i2]
+        
+#         return calc(0,n-1)
 
 # class Solution:
 #     def longestPalindromeSubseq(self, s: str) -> int:
