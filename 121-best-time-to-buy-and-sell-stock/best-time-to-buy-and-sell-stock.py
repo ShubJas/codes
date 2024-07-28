dd8101052ccb3b59a7c5ed1c3c66b04784855e7d
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-07-28 23:17:59 +0530

    Time: 776 ms (27.76%) | Memory: 27.5 MB (24.30%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -3,11 +3,11 @@ class Solution:
 
 
         n = len(prices)
-        min_p = float('inf')
+        min_p = prices[0]
         profit = 0
         for i in range(1,n):
-            min_p = min(min_p,prices[i-1])
             profit = max(profit,prices[i]-min_p)
+            min_p = min(min_p,prices[i])
         
         return profit
 
