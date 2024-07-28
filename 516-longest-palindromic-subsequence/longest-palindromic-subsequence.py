96de81ca084effbef20b245920c141ade659c056
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-07-28 14:44:54 +0530

    Time: 1159 ms (48.71%) | Memory: 16.6 MB (93.77%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.py
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.py
@@ -1,5 +1,5 @@
-#  s1 = s , s2 = rev(s) , now lcs in them is longest pal
 #  M1
+#  s1 = s , s2 = rev(s) , now lcs in them is longest pal
 
 class Solution:
     def longestPalindromeSubseq(self, s: str) -> int:
@@ -22,6 +22,29 @@ class Solution:
             prev = curr
         
         return prev[n]
+
+# class Solution:
+#     def longestPalindromeSubseq(self, s: str) -> int:
+
+#         n = len(s)
+#         t = s[::-1]
+
+#         # Initialize dp array
+#         prev = [0] * (n + 1) 
+
+#         # Fill the dp array
+#         for i1 in range(1, n + 1):
+#             curr = [0] * (n + 1) 
+
+#             for i2 in range(1, n + 1):
+#                 if s[i1 - 1] == t[i2 - 1]:
+#                     curr[i2] = 1 + prev[i2 - 1]
+#                 else:
+#                     curr[i2] = max(prev[i2], curr[i2 - 1])
+#             prev = curr
+        
+#         return prev[n]
+
 
 # class Solution:
 #     def longestPalindromeSubseq(self, s: str) -> int:
