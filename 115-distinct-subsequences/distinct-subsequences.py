4390baca084ec8c48d7ebccc19140a784e6ab453
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-07-28 18:14:58 +0530

    Time: 218 ms (84.48%) | Memory: 16.6 MB (96.47%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.py
+++ 115-distinct-subsequences/distinct-subsequences.py
@@ -6,26 +6,56 @@ class Solution:
         n2 = len(t)
 
 
-        dp = [[0] * (n2+1) for _ in range(n1+1)]
+        prev = [0] * (n2+1) 
 
 
         for i2 in range(n2+1):
-            dp[0][i2] = 0
+            prev[i2] = 0
         
-        for i1 in range(n1+1):
-            dp[i1][0] = 1
+        prev[0] = 1
 
         for i1 in range(1,n1+1):
+            curr = [0] * (n2+1) 
+            curr[0] = 1
 
             for i2 in range(1,n2+1):
 
                 if s[i1-1] == t[i2-1]:
-                    dp[i1][i2] =  dp[i1-1][i2-1] + dp[i1-1][i2]
+                    curr[i2] =  prev[i2-1] + prev[i2]
                 else:
-                    dp[i1][i2] = dp[i1-1][i2] 
+                    curr[i2] = prev[i2] 
+            prev = curr
         
-        return dp[n1][n2]
+        return prev[n2]
 
+
+# class Solution:
+#     def numDistinct(self, s: str, t: str) -> int:
+
+
+#         n1 = len(s)
+#         n2 = len(t)
+
+
+#         dp = [[0] * (n2+1) for _ in range(n1+1)]
+
+
+#         for i2 in range(n2+1):
+#             dp[0][i2] = 0
+        
+#         for i1 in range(n1+1):
+#             dp[i1][0] = 1
+
+#         for i1 in range(1,n1+1):
+
+#             for i2 in range(1,n2+1):
+
+#                 if s[i1-1] == t[i2-1]:
+#                     dp[i1][i2] =  dp[i1-1][i2-1] + dp[i1-1][i2]
+#                 else:
+#                     dp[i1][i2] = dp[i1-1][i2] 
+        
+#         return dp[n1][n2]
 
 
 # class Solution:
