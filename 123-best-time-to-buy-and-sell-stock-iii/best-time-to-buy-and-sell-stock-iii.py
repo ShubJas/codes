f3a552bf0a9a285652ff73f0dcfbe867f635aa8f
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-07-29 10:11:10 +0530

    Time: 1843 ms (23.31%) | Memory: 56.7 MB (28.83%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
@@ -1,36 +1,64 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
 
-
         
         n = len(prices)
         
-        dp = [[[-1 for _ in range(3)] for _ in range(2)] for _ in range(n)]
+        dp = [[[0 for _ in range(3)] for _ in range(2)] for _ in range(n+1)]
 
-        def calc(i,buy,count):
+        for i in range(n-1,-1,-1):
 
+            for buy in range(2):
 
-            if count == 2 or i == n:
-                return 0
-            
-
-            if dp[i][buy][count] != -1:
-                return dp[i][buy][count]
-
-            if buy == 1:
-                take = -prices[i] + calc(i+1,0,count)
-                ntake = calc(i+1,1,count)
-                result = max(take,ntake)
-            
-            else:
-                sell = prices[i] + calc(i+1,1,count+1)
-                nsell = calc(i+1,0,count)
-                result = max(sell,nsell)
-            
-            dp[i][buy][count] = result
-            return dp[i][buy][count]
+                for count in range(1,-1,-1):
+                    if buy == 1:
+                        take = -prices[i] + dp[i+1][0][count]
+                        ntake = dp[i+1][1][count]
+                        result = max(take,ntake)
+                    
+                    else:
+                        sell = prices[i] + dp[i+1][1][count+1]
+                        nsell = dp[i+1][0][count]
+                        result = max(sell,nsell)
+                    
+                    dp[i][buy][count] = result
         
-        return calc(0,1,0)
+        return dp[0][1][0]
+
+
+
+# class Solution:
+#     def maxProfit(self, prices: List[int]) -> int:
+
+        
+#         n = len(prices)
+        
+#         dp = [[[-1 for _ in range(3)] for _ in range(2)] for _ in range(n)]
+    
+#         def calc(i,buy,count):
+
+
+#             if count == 2 or i == n:
+#                 return 0
+            
+
+#             if dp[i][buy][count] != -1:
+#                 return dp[i][buy][count]
+
+#             if buy == 1:
+#                 take = -prices[i] + calc(i+1,0,count)
+#                 ntake = calc(i+1,1,count)
+#                 result = max(take,ntake)
+            
+#             else:
+#                 sell = prices[i] + calc(i+1,1,count+1)
+#                 nsell = calc(i+1,0,count)
+#                 result = max(sell,nsell)
+            
+#             dp[i][buy][count] = result
+#             return dp[i][buy][count]
+        
+#         return calc(0,1,0)
 
 
 # class Solution:
