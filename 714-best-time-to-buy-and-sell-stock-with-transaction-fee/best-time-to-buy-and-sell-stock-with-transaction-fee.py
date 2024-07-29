1509316f09158f6e0c4dde98fe076d514794c9d9
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-07-29 13:26:19 +0530

    Time: 636 ms (36.03%) | Memory: 23.6 MB (77.24%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -3,28 +3,58 @@ class Solution:
 
         n = len(prices)
 
-        dp = [[0] * 2 for _ in range(n+1)]
+        ahead = [0] * 2 
 
 
 
 
         for i in range(n-1,-1,-1):
+            curr = [0] * 2 
 
             for buy in range(2):
                 if buy == 1:
-                    take = -prices[i] + dp[i+1][0]
-                    ntake = dp[i+1][1]
+                    take = -prices[i] + ahead[0]
+                    ntake = ahead[1]
                     result =  max(take,ntake)
                 else:
-                    sell = prices[i] - fee + dp[i+1][1]
-                    nsell = dp[i+1][0]
+                    sell = prices[i] - fee + ahead[1]
+                    nsell = ahead[0]
                     result =  max(sell,nsell)
                 
-                dp[i][buy] = result
+                curr[buy] = result
+            
+            ahead = curr
 
 
-        return dp[0][1]
+        return ahead[1]
 
+
+# class Solution:
+#     def maxProfit(self, prices: List[int], fee: int) -> int:
+
+#         n = len(prices)
+
+#         dp = [[0] * 2 for _ in range(n+1)]
+
+
+
+
+#         for i in range(n-1,-1,-1):
+
+#             for buy in range(2):
+#                 if buy == 1:
+#                     take = -prices[i] + dp[i+1][0]
+#                     ntake = dp[i+1][1]
+#                     result =  max(take,ntake)
+#                 else:
+#                     sell = prices[i] - fee + dp[i+1][1]
+#                     nsell = dp[i+1][0]
+#                     result =  max(sell,nsell)
+                
+#                 dp[i][buy] = result
+
+
+#         return dp[0][1]
 
 # class Solution:
 #     def maxProfit(self, prices: List[int], fee: int) -> int:
