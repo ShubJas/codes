31d96ff4fef7f32791ba559d590d5df0a7ed7469
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-08-03 11:49:52 +0530

    Time: 769 ms (5.00%) | Memory: 38.3 MB (11.37%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -1,30 +1,61 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         n = len(nums)
-        dp = [[None] * 2 for _ in range(n)]
+        dp = [[None] * 2 for _ in range(n+1)]
 
-        def calc(i, must_take):
-            if i < 0:
-                return 0 if must_take else -float('inf')
 
-            if dp[i][must_take] is not None:
-                return dp[i][must_take]
+        dp[0][True] = 0
+        dp[0][False] = -float('inf')
 
-            # Consider taking the current element
-            pick = nums[i] + calc(i - 1, True)
 
-            # Consider not taking the current element
-            if must_take:
-                npick = 0  # If we must take, then npick is 0
-            else:
-                npick = calc(i - 1, False)
+        for i in range(1,n+1):
 
-            dp[i][must_take] = max(pick, npick)
-            return dp[i][must_take]
+            for must_take in [True,False]:
 
-        return calc(n - 1, False)
+                # Consider taking the current element
+                pick = nums[i-1] + dp[i - 1][True]
 
-            
+                # Consider not taking the current element
+                if must_take:
+                    npick = 0  # If we must take, then npick is 0
+                else:
+                    npick = dp[i - 1][False]
+
+                dp[i][must_take] = max(pick, npick)
+
+        return dp[n][False]
+#  i -> n-1 to 0
+#  0 base case
+#  n to 1
+
+
+# class Solution:
+#     def maxSubArray(self, nums: List[int]) -> int:
+#         n = len(nums)
+#         dp = [[None] * 2 for _ in range(n)]
+
+#         def calc(i, must_take):
+#             if i < 0:
+#                 return 0 if must_take else -float('inf')
+
+#             if dp[i][must_take] is not None:
+#                 return dp[i][must_take]
+
+#             # Consider taking the current element
+#             pick = nums[i] + calc(i - 1, True)
+
+#             # Consider not taking the current element
+#             if must_take:
+#                 npick = 0  # If we must take, then npick is 0
+#             else:
+#                 npick = calc(i - 1, False)
+
+#             dp[i][must_take] = max(pick, npick)
+#             return dp[i][must_take]
+
+#         return calc(n - 1, False)
+
+
 # class Solution:
 #     def maxSubArray(self, nums: List[int]) -> int:
 
