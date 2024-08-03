f21555caef16a6a90bc02fa9f48a3830debc0e3a
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-08-03 13:33:39 +0530

    Time: 169 ms (64.17%) | Memory: 18 MB (16.20%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,19 +1,28 @@
+#  Cancellation algo/ Boyer Moore’s Voting Algorithm
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
 
         n  = len(nums)
+        # as n/2 only one majority ele psbl 
+        #  so one var
+
         appear = n // 2
 
-        count = defaultdict(int)
-
-        for x in nums:
-
-            count[x] += 1
+        count = 0
+        ele = 0
         
-        result = []
-        for x,cnt in count.items():
-            if cnt > appear:
-                return x
+        for x in nums:
+            if count == 0:
+                ele = x
+                count +=1
+            elif x == ele:
+                count +=1
+            else:
+                count -=1
+        
+
+        if nums.count(ele) > appear:
+            return ele
 
 
 # class Solution:
