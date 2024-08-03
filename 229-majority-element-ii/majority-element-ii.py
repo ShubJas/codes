8cf2696658c29e6d86895419722bb5f0ec7855ce
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-08-03 13:43:22 +0530

    Time: 94 ms (81.45%) | Memory: 17.9 MB (14.29%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.py
+++ 229-majority-element-ii/majority-element-ii.py
@@ -1,20 +1,64 @@
+#  Cancellation algo/ Boyer Moore’s Voting Algorithm
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
 
         n  =len(nums)
-        appear = n //3
+        appear = n // 3
+        # as / 3 , there are 2 max elements possible so 2 vars
 
-        count = defaultdict(int)
+
+        count1 = count2 = 0 
+        ele1 = ele2 = -1
+
 
         for x in nums:
 
-            count[x] += 1
+            if count1 == 0 and x != ele2:
+                ele1 = x
+                count1 += 1
+
+            elif count2 == 0 and x != ele1:
+                ele2 = x
+                count2 += 1 
+
+            elif x == ele1:
+                count1 += 1
+            elif x == ele2:
+                count2 += 1
+            else:
+                count1 -= 1
+                count2 -= 1
         
-        result = []
-        for x,cnt in count.items():
-            if cnt > appear:
-                result.append(x)
+        result = set()
+
+        if nums.count(ele1) > appear:
+            result.add(ele1)
+
+        
+        if nums.count(ele2) > appear:
+            result.add(ele2)
 
 
-        return result
+        return list(result)
+
+# class Solution:
+#     def majorityElement(self, nums: List[int]) -> List[int]:
+
+#         n  =len(nums)
+#         appear = n //3
+
+#         count = defaultdict(int)
+
+#         for x in nums:
+
+#             count[x] += 1
+        
+#         result = []
+#         for x,cnt in count.items():
+#             if cnt > appear:
+#                 result.append(x)
+
+
+#         return result
+        
         
