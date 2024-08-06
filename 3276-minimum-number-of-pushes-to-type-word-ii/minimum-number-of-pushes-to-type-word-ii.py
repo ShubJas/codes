3caba08259c0a915f5ae1d3be572f7a3c401075a
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-08-06 22:01:45 +0530

    Time: 147 ms (73.03%) | Memory: 17.6 MB (36.75%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.py
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.py
@@ -1,30 +1,44 @@
 class Solution:
     def minimumPushes(self, word: str) -> int:
 
-        count = defaultdict(int)
-        mapper = defaultdict(int)
+        count = Counter(word)
+        
+        count_values = sorted(count.values(),reverse =True)
+        
 
 
-        for c in word:
-            count[c] +=1
-        
-        #  when u sort it becomes a list of tuples
-        count  = sorted(count.items(),key = lambda item: item[1],reverse = True) 
-        i= 1
+        return sum(count_values[:8]) + sum(count_values[8:16])* 2 + sum(count_values[16:24])* 3 + sum(count_values[24:])* 4  
 
-        pos = 1
-        for c,cnt in count:
-            # mapper[i] = (c,pos)
-            mapper[c] = pos
-            i += 1
-            if i == 9:
-                pos += 1
-                i = 1
+
+# class Solution:
+#     def minimumPushes(self, word: str) -> int:
+
+#         count = defaultdict(int)
+#         mapper = defaultdict(int)
+
+
+#         for c in word:
+#             count[c] +=1
         
-        result = 0
-        for c in word:
-            result += mapper[c]
+#         #  when u sort it becomes a list of tuples
+#         count  = sorted(count.items(),key = lambda item: item[1],reverse = True) 
+#         i= 1
+#         pos = 1
+#         for c,cnt in count:
+#             mapper[c] = pos
+#             i += 1
+#             if i == 9:
+#                 pos += 1
+#                 i = 1
         
-        return result
+#         result = 0
+#         for c in word:
+#             result += mapper[c]
+        
+#         return result
+
+
+
+
 
         
