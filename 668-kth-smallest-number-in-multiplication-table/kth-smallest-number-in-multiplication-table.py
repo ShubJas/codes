c308f7a5574b1cddd5ec5c712300b4be0e370794
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-08-07 15:48:23 +0530

    Time: 283 ms (95.81%) | Memory: 16.5 MB (20.69%) - LeetSync

--- 668-kth-smallest-number-in-multiplication-table/kth-smallest-number-in-multiplication-table.py
+++ 668-kth-smallest-number-in-multiplication-table/kth-smallest-number-in-multiplication-table.py
@@ -1,33 +1,49 @@
 class Solution:
     def findKthNumber(self, m: int, n: int, k: int) -> int:
+        
 
-
-        def issmallerthan(num) -> bool:
-            
+        def feasible(num):
             count = 0
-            for div in range(1,m+1):
-                if num//div > n:
-                    count += n
+            for row in range(1,m+1):
+
+                if num//row > n:
+                    count+=n
                 else:
-                    count+= num//div
-                # count += min( num//div , n)
-                if count ==0:
-                    break
-
-            return count >= k
-
+                    count+= num//row
+            
+            return count>=k
 
 
         l = 1
-        r = m * n
+        r = m*n
 
-        while l< r:
-            mid = (l + r)//2
-
-            if issmallerthan(mid):
+        while l<r:
+            mid = (l+r)//2
+            if feasible(mid):
                 r = mid
             else:
-                l = mid + 1
+                l = mid+1
         
         return l
-        
+
+# import heapq
+
+# class Solution:
+#     def findKthNumber(self, m: int, n: int, k: int) -> int:
+#         # Generate all elements in the multiplication table
+#         arr = []
+
+#         for r in range(1, m + 1):
+#             for c in range(1, n + 1):
+#                 arr.append(r * c)
+
+#         # Transform the list into a heap
+#         heapq.heapify(arr)
+
+#         # Pop elements k-1 times to get the k-th smallest element
+#         while k > 1:
+#             heapq.heappop(arr)
+#             k -= 1
+
+#         # Return the k-th smallest element
+#         return heapq.heappop(arr)
