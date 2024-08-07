d7c12d6d63f5469bb341975a455be6852ae28da6
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-08-07 14:26:11 +0530

    Time: 268 ms (52.44%) | Memory: 18 MB (96.74%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -2,23 +2,27 @@ class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
 
 
-        def feasible(speed) -> bool:
-            hours = 0
-            for p in piles:
-                hours+= (p-1)//speed + 1
-            if hours > h:
-                 return False
+        def feasible(eat_rate):
+            hours = 0         
+            for bananas in piles:
+
+                hours += ((bananas-1) // eat_rate ) + 1
+                if hours > h:
+                    return False
+            
             return True
 
-        l= 1
+        
+
+
+        l = 1
         r = max(piles)
 
-        while( l < r):
-            m = (l+r)//2
-
-            if(feasible(m)):
+        while l<r:
+            m  =(l+r)//2
+            if feasible(m):
                 r = m
             else:
                 l = m + 1
-        return l
         
+        return l
