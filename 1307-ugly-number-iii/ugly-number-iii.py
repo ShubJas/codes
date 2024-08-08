bb2ad5e82a1d981db2a3abad76c325cca3e7075a
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-08-08 15:01:17 +0530

    Time: 33 ms (77.88%) | Memory: 16.7 MB (17.11%) - LeetSync

--- 1307-ugly-number-iii/ugly-number-iii.py
+++ 1307-ugly-number-iii/ugly-number-iii.py
@@ -1,42 +1,31 @@
+# least common multiples (LCM) and the inclusion-exclusion principle
 class Solution:
     def nthUglyNumber(self, n: int, a: int, b: int, c: int) -> int:
 
-        def lcm(x,y):
-
-            return x*y // math.gcd(x,y)
-
-        def feasible(num):
-            count = num//a + num// b + num//c
-            count-= num//lcm_ab + num//lcm_ac +num//lcm_bc
-            count+= num//lcm_abc
-
-            return count>=n
-
-        
-        lcm_ab = lcm(a,b)
-        lcm_ac = lcm(a,c)
-        lcm_bc = lcm(b,c)
-        lcm_abc = lcm(lcm_ab,c)
-
-
+        def lcm(x, y):
+            return x * y // math.gcd(x, y)
         
 
-
-
-
-
+        def feasible(num) -> bool:
+            count = num // a + num // b + num // c  # count of each psble a,b,c
+            count -= num // lcm_ab + num // lcm_ac + num // lcm_bc   # remove overcountsn of each psble a,b,c
+            count += num // lcm_abc # add the count removed of all of them
+            return count >= n
+        
+        lcm_ab = lcm(a, b)
+        lcm_bc = lcm(b, c)
+        lcm_ac = lcm(a, c)
+        lcm_abc = lcm(lcm_ab, c)
 
         l = 1
-        r = n * min(a,b,c)
-
-
-        while l < r:
-
-            mid = (l+r)//2
-            if feasible(mid):
-                r = mid
-            else:
-                l = mid + 1
+        r= n * min(a,b,c)
         
+        while l<r:
+            m = (l+r)//2
+
+            if feasible(m):
+                r = m
+            else:
+                l = m+1
         return l
         
