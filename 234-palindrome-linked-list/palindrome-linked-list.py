7d2dd066f8e764bfa30f1f75a7cce9b54db2e849
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-08-10 22:09:17 +0530

    Time: 257 ms (98.87%) | Memory: 27.5 MB (96.58%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.py
+++ 234-palindrome-linked-list/palindrome-linked-list.py
@@ -5,52 +5,27 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        
-        fast = slow = head
-        rev =None
 
+
+        fast = head
+        slow = head
+    
+        rev = None
         while fast and fast.next:
             fast = fast.next.next
-            rev , rev.next , slow = slow , rev  ,slow.next
-
-        #  For odd number skip mid
+            rev , rev.next, slow = slow , rev , slow.next
+        
         if fast:
             slow = slow.next
+        
 
         
+
+
         while rev and slow:
             if rev.val != slow.val:
                 return False
-            rev = rev.next
-            slow = slow.next
+            rev= rev.next
+            slow= slow.next
         
         return True
-
-
-
-        # def reverse(node):
-        #     prev = None
-        #     curr =node
-        #     while curr:
-        #         forw = curr.next
-        #         curr.next = prev
-        #         prev = curr
-        #         curr = forw
-        #     return prev
-        
-        # fast = head
-        # slow = head
-        # while fast and fast.next:
-        #     slow = slow.next
-        #     fast = fast.next.next
-        
-        # slow = reverse(slow)
-
-        # while head and slow:
-        #     if head.val != slow.val:
-        #         return False
-        #     head = head.next
-        #     slow = slow.next
-        
-        # return True
-        
