166f5558974e23a8403c910c73bbd07904ac057f
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-08-13 19:02:33 +0530

    Time: 33 ms (91.81%) | Memory: 17.2 MB (68.44%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
@@ -6,54 +6,22 @@
 #         self.right = right
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        
+
+        q = collections.deque()
+        
+        q.append(root)
         result = []
-        q = collections.deque([root])
-
-
         while q:
-            temp = []
-            for _ in range(len(q)):
+            qlen = len(q)
+            level = []
+            for _ in range(qlen):
                 curr = q.popleft()
                 if curr:
-                    temp.append(curr.val)
+                    level.append(curr.val)
                     q.append(curr.left)
                     q.append(curr.right)
             if q:
-                result.append(temp)
+                result.append(level)
+
         return result
-
-
-        
-
-
-
-# # Definition for a binary tree node.
-# # class TreeNode:
-# #     def __init__(self, val=0, left=None, right=None):
-# #         self.val = val
-# #         self.left = left
-# #         self.right = right
-# class Solution:
-#     def __init__(self):
-#         self.result = []
-#     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-#         Q = deque([root])
-#         if not root:
-#             return None
-#         while Q:
-#             temp = []
-#             sub_list_length = len(Q)
-#             for _ in range(sub_list_length):
-#                 curr = Q.popleft()
-#                 if curr.left:
-#                     Q.append(curr.left)
-#                 if curr.right:
-#                     Q.append(curr.right)
-#                 temp.append(curr.val)
-#             self.result.append(temp)
-        
-#         return self.result
-
-
-
-        
