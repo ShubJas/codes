ec392f66db0fb9ee78025cf1b13823b1b150deab
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-08-13 11:56:22 +0530

    Time: 157 ms (61.08%) | Memory: 28.1 MB (23.65%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -1,48 +1,33 @@
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
-        
-        n = len(nums)
-        k = k%n
+        """
+        Do not return anything, modify nums in-place instead.
+        """
 
         nums.reverse()
-        nums[:k] = reversed(nums[:k])
+
+        n = len(nums)
+        k = k % n 
+
         nums[k:] = reversed(nums[k:])
+        nums[:k] = reversed(nums[:k])
+        return nums
 
-
-
-
-
-        # def rev(l,r):
-        #     while l<r:
-        #         nums[l] , nums[r] = nums[r] , nums[l]
-        #         l+=1
-        #         r-=1
-        
         # n = len(nums)
-        # k = k%n
-        # rev(0,n-1)
-        # rev(0,k-1)
-        # rev(k,n-1)
-            
 
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        # n = len(nums)
-        # k = k % n  
+        # rot = k % n 
 
-        # def rot():
-        #     last = nums[-1]
-        #     for i in range(len(nums) - 1, 0, -1):
-        #         nums[i] = nums[i - 1]
+
+        # def rotate(nums):
+        #     last = nums[n-1]
+        #     for i in range(n-1,0,-1):
+        #         nums[i] = nums[i-1]
         #     nums[0] = last
+
+
+        # for i in range(rot):
+        #     rotate(nums)
         
-        # for _ in range(k):
-        #     rot()  
+
+        # return nums
+        
