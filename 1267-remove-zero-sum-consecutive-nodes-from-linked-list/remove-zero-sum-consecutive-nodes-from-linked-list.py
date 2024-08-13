3133b7fbe1ddbb104ce9a71a33b817082bbda3fa
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-08-13 18:25:04 +0530

    Time: 44 ms (51.69%) | Memory: 16.7 MB (65.29%) - LeetSync

--- 1267-remove-zero-sum-consecutive-nodes-from-linked-list/remove-zero-sum-consecutive-nodes-from-linked-list.py
+++ 1267-remove-zero-sum-consecutive-nodes-from-linked-list/remove-zero-sum-consecutive-nodes-from-linked-list.py
@@ -3,36 +3,28 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
-# If psum exists in the hashmap, it means that the sublist between the node stored in hashmap[psum] and the current node has a sum of zero.
 class Solution:
     def removeZeroSumSublists(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
 
         hashmap = defaultdict(ListNode)
         dummy = ListNode(0,head)
-        curr = dummy    
         psum = 0
+        curr = dummy
+
         while curr:
             psum += curr.val
             if psum in hashmap:
-                node = hashmap[psum].next
-                temp_sum = psum
-                while node != curr:
-                    temp_sum += node.val
-                    if temp_sum in hashmap:
-                        del hashmap[temp_sum] 
-                    node = node.next
+                temp = hashmap[psum].next
+                temppsum = psum
+                while temp != curr:
+                    temppsum += temp.val
+                    if temppsum in hashmap:
+                        del hashmap[temppsum]
+                    temp = temp.next
                 hashmap[psum].next = curr.next
             else:
                 hashmap[psum] = curr
             curr = curr.next
         
         return dummy.next
-
-        
-            
-            
-
-
-
-        
