8467fb71951c4bb9f4153d94ea55a264e7ebe0b8
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-08-19 11:35:31 +0530

    Time: 82 ms (82.72%) | Memory: 16.6 MB (71.96%) - LeetSync

--- 264-ugly-number-ii/ugly-number-ii.py
+++ 264-ugly-number-ii/ugly-number-ii.py
@@ -1,15 +1,15 @@
 class Solution:
     def nthUglyNumber(self, n: int) -> int:
         
-
+        i5 = i2 = i3 = 0
 
         dp = [1] * n
-        i2 = i3 = i5 = 0
+        for i in range(1,n):
 
-        for i  in range(1,n):
-            p2 = dp[i2] * 2
-            p3 = dp[i3] * 3
+            p2 = dp[i2]*2
+            p3 = dp[i3]* 3
             p5 = dp[i5] * 5
+
             dp[i] = min(p2,min(p3,p5))
 
             if dp[i] == p2:
@@ -19,4 +19,6 @@ class Solution:
             if dp[i] == p5:
                 i5+=1
         
+
         return dp[n-1]
+                
