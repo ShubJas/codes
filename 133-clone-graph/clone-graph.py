d3e102dbdeb85c2417aa5f88ef8a239ae7a2ba87
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-08-21 07:48:48 +0530

    Time: 41 ms (51.52%) | Memory: 16.8 MB (70.74%) - LeetSync

--- 133-clone-graph/clone-graph.py
+++ 133-clone-graph/clone-graph.py
@@ -10,33 +10,21 @@ from typing import Optional
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
         
-
         if not node:
-            return None
-            
-        # Dictionary to map original nodes to their clones
-        old_new = {}
+            return node
 
+        old_new = {}
         def dfs(node):
 
-            # If the node has already been cloned, return the clone from the map
+
             if node in old_new:
                 return old_new[node]
-
-            # Clone the current node (only the value, neighbors will be added later)
-            newNode = Node(node.val)
             
-            # Map the original node to its clone
-            old_new[node] = newNode
-            
-            # Iterate over all the neighbors of the current node
-            for neig in node.neighbors:
-                # Recursively clone the neighbors and add them to the clone's neighbors
-                newNode.neighbors.append(dfs(neig))
+            old_new[node] = Node(node.val)
 
-            # Return the cloned node
-            return newNode
+            for neighbor in node.neighbors:
+                old_new[node].neighbors.append(dfs(neighbor))
+            
+            return old_new[node]
         
-        # If the input node is None, return None
-        # Otherwise, start the DFS from the input node
         return dfs(node)
