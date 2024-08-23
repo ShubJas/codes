74aeb2f0f9e5c41bd32000295b938bd9d6ef5e1d
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-08-23 12:01:02 +0530

    Time: 68 ms (39.66%) | Memory: 17.1 MB (59.43%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
@@ -3,41 +3,26 @@ class Solution:
 
 
         stack = []
-        op = ['*','+','-','/']
-        op = set(op)
-
-        for x in tokens:
-
-            if x not in op:
-                stack.append(x)
-            
+        for token in tokens:
+            if token == '+':
+                b = stack.pop()
+                a = stack.pop()
+                stack.append(int(a) + int(b))
+            elif token == '-':
+                b = stack.pop()
+                a = stack.pop()
+                stack.append(int(a) - int(b))
+            elif token == '*':
+                b = stack.pop()
+                a = stack.pop()
+                stack.append(int(a) * int(b))
+            elif token == '/':
+                b = stack.pop()
+                a = stack.pop()
+                stack.append(int(int(a) / int(b)))
             else:
-                n1 = int(stack.pop())
-                n2 = int(stack.pop())
-                print()
-                print(n2)
-                print(x)
-                print(n1)
+                stack.append(token)
 
-                print()
-
-                ans = 0
-                if x == '*':
-                    ans = n2 * n1
-                elif x == '/':
-                    if n1 < 0 and n2<0:
-                        ans = abs(n2) // abs(n1)
-                    elif n1 < 0 or n2 < 0:
-                        ans = abs(n2) // abs(n1)
-                        ans = -ans
-                    else:
-                        ans = n2 // n1
-                elif x == '+':
-                    ans = n2 + n1
-                else:
-                    ans = n2 - n1
-                
-                stack.append(str(ans))
-        
-        return int(stack.pop())
+            # print(stack)
+        return int(stack[0])
         
