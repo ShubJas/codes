56f1af022701735f1c50fa32f04d4943276847d6
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-08-23 12:26:24 +0530

    Time: 43 ms (10.30%) | Memory: 16.4 MB (93.57%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.py
+++ 191-number-of-1-bits/number-of-1-bits.py
@@ -1,4 +1,25 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
+        count = 0
+        while n:
+
+            n  &= n-1
+            count+=1
         
-        return list(bin(n)[2:]).count('1')
+        return count
+
+
+
+
+'''
+Suppose n = 12, which in binary is 1100.
+n - 1 = 11, which in binary is 1011.
+n & (n - 1) = 1100 & 1011 = 1000 (in binary), which is 8 in decimal.
+The lowest set bit has been removed, and now n is 1000.
+
+'''
+
+# class Solution:
+#     def hammingWeight(self, n: int) -> int:
+        
+#         return list(bin(n)[2:]).count('1')
