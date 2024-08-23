550527cec793a13546698b4a06515f2c9708342c
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-08-23 10:31:47 +0530

    Time: 536 ms (99.68%) | Memory: 23.1 MB (39.29%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
@@ -3,16 +3,32 @@ class Solution:
         
 
 
-        def dist(p1,points):
+        def dist(p1):
             return pow(p1[0],2)+pow(p1[1],2)
         
 
-        pq = []
-        for p in points:
-            heapq.heappush(pq,(dist(p,points),p))
+        points = sorted(points,key = lambda p : dist(p))
+
+        return points[:k]
+
         
-        result = []
-        for _ in range(k):
-            _, p = heapq.heappop(pq)
-            result.append(p)
-        return result
+
+
+# class Solution:
+#     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        
+
+
+#         def dist(p1,points):
+#             return pow(p1[0],2)+pow(p1[1],2)
+        
+
+#         pq = []
+#         for p in points:
+#             heapq.heappush(pq,(dist(p,points),p))
+        
+#         result = []
+#         for _ in range(k):
+#             _, p = heapq.heappop(pq)
+#             result.append(p)
+#         return result
