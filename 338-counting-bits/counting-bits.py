fcd7b5531836c7f69c8533873912eacc022e92c1
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-08-25 11:38:32 +0530

    Time: 58 ms (77.56%) | Memory: 23.3 MB (42.62%) - LeetSync

--- 338-counting-bits/counting-bits.py
+++ 338-counting-bits/counting-bits.py
@@ -1,3 +1,16 @@
+'''
+Even i: If i is even, then its binary representation is simply the binary representation of i // 2 shifted left by one bit (which is equivalent to multiplying by 2). Shifting left by one bit does not add any new 1's. Therefore, result[i] = result[i // 2].
+
+Example:
+4 (binary: 100) is 2 (binary: 10) shifted left by one bit. The count of 1's is the same: 1.
+result[4] = result[2] = 1.
+Odd i: If i is odd, then its binary representation is the same as i // 2 shifted left by one bit, plus a 1 in the least significant bit (rightmost position). Therefore, result[i] = result[i // 2] + 1.
+
+Example:
+5 (binary: 101) is 2 (binary: 10) shifted left by one bit, with an additional 1 added at the end. Therefore, result[5] = result[2] + 1 = 1 + 1 = 2.
+
+
+'''
 class Solution:
     def countBits(self, n: int) -> List[int]:
 
