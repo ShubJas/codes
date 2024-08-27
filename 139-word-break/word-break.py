17ae6a92b384f3920f5246642ac5664d74edaa99
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-08-27 11:28:44 +0530

    Time: 44 ms (32.78%) | Memory: 16.7 MB (50.01%) - LeetSync

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -1,19 +1,52 @@
+# # start -> 0 to n+1
+# class Solution:
+#     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        
+#         n = len(s)
+
+#         dp = [False] * (n+1)
+#         dp[n] = True
+#         words = set(wordDict)
+
+#         for start in range(n-1, -1, -1):
+#             word = ''
+#             for i in range(start, n):
+#                 word += s[i]
+#                 if word in words:
+#                     if dp[i + 1]:
+#                         dp[start] = True
+#                         break  # No need to check further, as we found a valid segmentation
+
+#         return dp[0]
+
+
+
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         
-        n = len(s)
+        n = len(s) 
 
-        dp = [False] * (n+1)
-        dp[n] = True
+        dp = [None] * (n+1)
+
         words = set(wordDict)
+        def calc(start):
 
-        for start in range(n-1, -1, -1):
+            if start >= n:
+                return True
+            
+            if dp[start] is not None:
+                return dp[start]
+
             word = ''
-            for i in range(start, n):
+            for i in range(start,n):
                 word += s[i]
                 if word in words:
-                    if dp[i + 1]:
-                        dp[start] = True
-                        break  # No need to check further, as we found a valid segmentation
+                    dp[start] = calc(i+1)
+                    if dp[start]:
+                        return True
+            
+            return False
+        
 
-        return dp[0]
+        return calc(0)
+
