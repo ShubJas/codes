2e2f1bc21da012e5a5354123fe88046426a75acf
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-08-27 11:18:51 +0530

    Time: 31 ms (95.21%) | Memory: 16.7 MB (50.01%) - LeetSync

---
+++ 139-word-break/word-break.py
@@ -0,0 +1,29 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        
+        n = len(s) 
+
+        dp = [None] * (n+1)
+
+        words = set(wordDict)
+        def calc(start):
+
+            if start >= n:
+                return True
+            
+            if dp[start] is not None:
+                return dp[start]
+
+            word = ''
+            for i in range(start,n):
+                word += s[i]
+                if word in words:
+                    dp[i+1] = calc(i+1)
+                    if dp[i+1]:
+                        return True
+            
+            return False
+        
+
+        return calc(0)
+
