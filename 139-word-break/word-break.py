1fd9fa2c3463151ed768dcaf6118d3d763996326
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-08-27 11:27:07 +0530

    Time: 37 ms (75.61%) | Memory: 16.7 MB (50.01%) - LeetSync

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -1,30 +1,19 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         
-        n = len(s) 
+        n = len(s)
 
-        dp = [None] * (n+1)
-
+        dp = [False] * (n+1)
+        dp[n] = True
         words = set(wordDict)
-        def calc(start):
 
-            if start >= n:
-                return True
-            
-            if dp[start] is not None:
-                return dp[start]
-
+        for start in range(n-1, -1, -1):
             word = ''
-            for i in range(start,n):
+            for i in range(start, n):
                 word += s[i]
                 if word in words:
-                    dp[i+1] = calc(i+1)
-                    if dp[i+1]:
-                        return True
-            
-            return False
-        
+                    if dp[i + 1]:
+                        dp[start] = True
+                        break  # No need to check further, as we found a valid segmentation
 
-        return calc(0)
-
-
+        return dp[0]
