d68cfd5f036b0d17833031d5751784f5642c0b43
ShubJas <100420318+ShubJas@users.noreply.github.com>
2024-08-30 16:28:23 +0530

    Time: 41 ms (23.92%) | Memory: 16.4 MB (81.42%) - LeetSync

--- 190-reverse-bits/reverse-bits.py
+++ 190-reverse-bits/reverse-bits.py
@@ -1,9 +1,20 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
-
+     
+        # Initialize result to 0
+        # This will hold the final reversed bits as we build it bit by bit.
         result = 0
+
+        # Iterate over all 32 bits of the input integer 'n'
         for i in range(32):
+            # Extract the i-th bit from 'n'
+            # (n >> i) shifts the bits of 'n' to the right by 'i' positions.
+            # & 1 isolates the least significant bit (the rightmost bit) after the shift.
             bit = (n >> i) & 1
-            result = result |  (bit <<(31 - i))
+
+            # Place the extracted bit in the reversed position in 'result'
+            # 'bit << (31 - i)' shifts the bit to its new reversed position.
+            result = result | (bit << (31 - i))
         
+        # Return the final result, which now contains the bits of 'n' reversed.
         return result
