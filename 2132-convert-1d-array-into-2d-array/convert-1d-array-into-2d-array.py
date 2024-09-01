a693e051d9e34c97f2ae9087bc6b26cadb8b4c7c
Shuaib Jawid Shahna <100420318+ShubJas@users.noreply.github.com>
2024-09-01 12:20:53 +0530

    Time: 704 ms (20.34%) | Memory: 23.8 MB (55.74%) - LeetSync

--- 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.py
+++ 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.py
@@ -2,14 +2,12 @@ from typing import List
 
 class Solution:
     def construct2DArray(self, original: List[int], m: int, n: int) -> List[List[int]]:
-        # Check if the total number of elements matches m * n
+        
         if len(original) != m * n:
             return []
 
-        # Initialize the result array
         result = [[-1] * n for _ in range(m)]
 
-        # Fill the result array
         for i in range(len(original)):
             col = i % n
             row = i // n
