3d747b53d2adf1d7648d7bf1f5ab48a9fad4ae02
Shuaib Jawid Shahna <100420318+ShubJas@users.noreply.github.com>
2024-09-01 21:02:46 +0530

    Time: 36 ms (63.16%) | Memory: 16.8 MB (25.90%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,24 +1,23 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-
         n = len(nums)
-
         result = []
 
-        def backtrack(i,stack):
-
-            print(i)
+        def backtrack(i, stack):
+            # Base case: If we've considered all elements
             if i == n:
-                result.append(stack[:])
+                result.append(stack[:])  # Append a copy of the current stack
                 return
             
+            # Include the current element
             stack.append(nums[i])
-            backtrack(i+1,stack)
+            backtrack(i + 1, stack)
+
+            # Exclude the current element
             stack.pop()
-            backtrack(i+1,stack)
+            backtrack(i + 1, stack)
 
-
-        backtrack(0,[])
-
+        # Start backtracking from index 0 with an empty stack
+        backtrack(0, [])
+        
         return result
-            
