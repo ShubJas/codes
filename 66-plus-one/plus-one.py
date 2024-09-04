dc8cfbaca8589a6490b4c712c3c5d4f4d9a3a842
Shuaib Jawid Shahna <100420318+ShubJas@users.noreply.github.com>
2024-09-04 20:05:20 +0530

    Time: 26 ms (97.87%) | Memory: 16.7 MB (23.27%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -1,6 +1,27 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         
+        carry = 1
+        digits = digits[::-1]
+        i = 0
+        n = len(digits)
+        while carry or i<n:
+            digit = digits[i] + carry 
+            carry = digit // 10
+            i+=1
+        
+        if carry:
+            digits.append(1)
+        
+        return digits[::-1]
+
+
+
+
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        
 
         digits = ''.join(str(n) for n in digits)
         digits = [int(digit) for digit in list(str(int(digits)+1))]
