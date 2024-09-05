91921be8fb6f6c16c744cd37d7311e92f843a825
Shuaib Jawid Shahna <100420318+ShubJas@users.noreply.github.com>
2024-09-05 22:33:41 +0530

    Time: 43 ms (7.04%) | Memory: 16.5 MB (83.89%) - LeetSync

---
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -0,0 +1,39 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        
+        if digits == "":
+            return []
+
+        keyboard = {
+        "2": "abc",
+        "3": "def",
+        "4": "ghi",
+        "5": "jkl",
+        "6": "mno",
+        "7": "pqrs",
+        "8": "tuv",
+        "9": "wxyz"
+        }
+
+        result = []
+        n = len(digits)
+
+        def backtrack(i,stack):
+
+            if i == n :
+                if len(stack) == n:
+                    result.append(''.join(stack))
+                return
+
+            for ch in keyboard[digits[i]]:
+                stack.append(ch)
+                backtrack(i+1,stack)
+                stack.pop()
+                backtrack(i+1,stack)
+            
+        backtrack(0,[])
+
+        return result
+
+
+                
