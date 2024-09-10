8f79c04a17cfe8d181c2d9986b08792ef392590e
Shuaib Jawid Shahna <100420318+ShubJas@users.noreply.github.com>
2024-09-10 23:13:43 +0530

    Time: 2132 ms (82.58%) | Memory: 31.9 MB (27.38%) - LeetSync

--- 3485-maximize-score-of-numbers-in-ranges/maximize-score-of-numbers-in-ranges.py
+++ 3485-maximize-score-of-numbers-in-ranges/maximize-score-of-numbers-in-ranges.py
@@ -6,35 +6,35 @@ class Solution:
         start.sort()
 
 
-        def feasible(mid): 
-            x = -inf
-            for s in start: 
-                x += mid
-                if x > s + d: 
-                    return False 
-                x = max(x, s)
-            return True
+        # def feasible(mid): 
+        #     x = -inf
+        #     for s in start: 
+        #         x += mid
+        #         if x > s + d: 
+        #             return False 
+        #         x = max(x, s)
+        #     return True
 
 
         # Feasibility function: checks if it's possible to make all differences
         # between consecutive elements at least 'target'
         #  Greedish type approach
-        # def feasible(target):
-        #     nums = start[:]  # Create a copy of the start array to modify
-        #     for i in range(1, n):
-        #         diff = nums[i] - nums[i - 1]  # Calculate the current difference
-        #         if diff >= target:
-        #             continue  # If the difference is already large enough, move to the next pair
+        def feasible(target):
+            nums = start[:]  # Create a copy of the start array to modify
+            for i in range(1, n):
+                diff = nums[i] - nums[i - 1]  # Calculate the current difference
+                if diff >= target:
+                    continue  # If the difference is already large enough, move to the next pair
                 
-        #         # Calculate the extra difference needed to reach the target
-        #         extra_needed = target - diff
-        #         # If the extra difference needed exceeds the interval [start[i], start[i] + d], return False
-        #         if extra_needed > d:
-        #             return False
-        #         # Otherwise, we adjust nums[i] to make the difference equal to 'target'
-        #         nums[i] += extra_needed
+                # Calculate the extra difference needed to reach the target
+                extra_needed = target - diff
+                # If the extra difference needed exceeds the interval [start[i], start[i] + d], return False
+                if extra_needed > d:
+                    return False
+                # Otherwise, we adjust nums[i] to make the difference equal to 'target'
+                nums[i] += extra_needed
             
-        #     return True  # If we can satisfy the minimum difference condition, return True
+            return True  # If we can satisfy the minimum difference condition, return True
 
         # Binary search range:
         # l is the smallest possible difference (0), r is the maximum possible difference.
