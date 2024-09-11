56d1e278c5c2a919efd1a1081a729c08061bf0d5
Shuaib Jawid Shahna <100420318+ShubJas@users.noreply.github.com>
2024-09-11 12:19:36 +0530

    Time: 96 ms (88.37%) | Memory: 17.5 MB (14.25%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -1,3 +1,5 @@
+#  See take u forward 3 solutions
+#  Space optimized (only one var)
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
         """
@@ -34,3 +36,29 @@ class Solution:
         if cextra == 0:
             for i in range(row):
                 matrix[i][0] = 0
+
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        row = len(matrix)
+        col = len(matrix[0])
+
+        # Use sets to keep track of rows and columns that need to be zeroed
+        zero_rows = set()
+        zero_cols = set()
+
+        # First pass: Identify the rows and columns that should be zeroed
+        for i in range(row):
+            for j in range(col):
+                if matrix[i][j] == 0:
+                    zero_rows.add(i)
+                    zero_cols.add(j)
+
+        # Second pass: Zero out the identified rows and columns
+        for i in range(row):
+            for j in range(col):
+                if i in zero_rows or j in zero_cols:
+                    matrix[i][j] = 0
+
