01dc900897900d20c9a12d21dadacaf7be7694e5
Shuaib Jawid Shahna <100420318+ShubJas@users.noreply.github.com>
2024-09-11 12:19:50 +0530

    Time: 102 ms (61.24%) | Memory: 17.5 MB (57.20%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -37,28 +37,28 @@ class Solution:
             for i in range(row):
                 matrix[i][0] = 0
 
-class Solution:
-    def setZeroes(self, matrix: List[List[int]]) -> None:
-        """
-        Do not return anything, modify matrix in-place instead.
-        """
-        row = len(matrix)
-        col = len(matrix[0])
+# class Solution:
+#     def setZeroes(self, matrix: List[List[int]]) -> None:
+#         """
+#         Do not return anything, modify matrix in-place instead.
+#         """
+#         row = len(matrix)
+#         col = len(matrix[0])
 
-        # Use sets to keep track of rows and columns that need to be zeroed
-        zero_rows = set()
-        zero_cols = set()
+#         # Use sets to keep track of rows and columns that need to be zeroed
+#         zero_rows = set()
+#         zero_cols = set()
 
-        # First pass: Identify the rows and columns that should be zeroed
-        for i in range(row):
-            for j in range(col):
-                if matrix[i][j] == 0:
-                    zero_rows.add(i)
-                    zero_cols.add(j)
+#         # First pass: Identify the rows and columns that should be zeroed
+#         for i in range(row):
+#             for j in range(col):
+#                 if matrix[i][j] == 0:
+#                     zero_rows.add(i)
+#                     zero_cols.add(j)
 
-        # Second pass: Zero out the identified rows and columns
-        for i in range(row):
-            for j in range(col):
-                if i in zero_rows or j in zero_cols:
-                    matrix[i][j] = 0
+#         # Second pass: Zero out the identified rows and columns
+#         for i in range(row):
+#             for j in range(col):
+#                 if i in zero_rows or j in zero_cols:
+#                     matrix[i][j] = 0
 
