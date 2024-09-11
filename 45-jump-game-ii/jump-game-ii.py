04b899b659a7c02cdb32893b4903371c5725b946
Shuaib Jawid Shahna <100420318+ShubJas@users.noreply.github.com>
2024-09-11 20:59:35 +0530

    Time: 3262 ms (16.81%) | Memory: 17.5 MB (77.54%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.py
+++ 45-jump-game-ii/jump-game-ii.py
@@ -2,27 +2,38 @@ class Solution:
     def jump(self, nums: List[int]) -> int:
         
         n = len(nums)
-        dp = [-1] * n
-        def calc(i):
+        dp = [float('inf')] * n
+        dp[n-1] = 0
 
-            if i >= n-1:
-                return 0
-            
-            if dp[i] != -1:
-                return dp[i]
-            
-            mini = float('inf')
+        for i in range(n-2,-1,-1):
             for j in range(i + 1,min(i+nums[i]+1,n)):
-                mini = min(mini,1 + calc(j))
-                
-            dp[i] = mini
+                dp[i]  = min(dp[i] ,1 + dp[j])
+        
+        return dp[0]
+    
+# class Solution:
+#     def jump(self, nums: List[int]) -> int:
+        
+#         n = len(nums)
+#         dp = [-1] * n
+#         def calc(i):
 
-            return dp[i]
+#             if i >= n-1:
+#                 return 0
+            
+#             if dp[i] != -1:
+#                 return dp[i]
+            
+#             mini = float('inf')
+#             for j in range(i + 1,min(i+nums[i]+1,n)):
+#                 mini = min(mini,1 + calc(j))
+                
+#             dp[i] = mini
+
+#             return dp[i]
         
 
-        return calc(0)
-    
-
+#         return calc(0)
 
 
 # class Solution:
