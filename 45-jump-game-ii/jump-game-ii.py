8b12e3d5ac269b9fc18e152c3ac309b8d176c46a
Shuaib Jawid Shahna <100420318+ShubJas@users.noreply.github.com>
2024-09-11 20:56:36 +0530

    Time: 5027 ms (7.11%) | Memory: 19.9 MB (5.60%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.py
@@ -0,0 +1,44 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        
+        n = len(nums)
+        dp = [-1] * n
+        def calc(i):
+
+            if i >= n-1:
+                return 0
+            
+            if dp[i] != -1:
+                return dp[i]
+            
+            mini = float('inf')
+            for j in range(i + 1,min(i+nums[i]+1,n)):
+                mini = min(mini,1 + calc(j))
+                
+            dp[i] = mini
+
+            return dp[i]
+        
+
+        return calc(0)
+    
+
+
+
+# class Solution:
+#     def jump(self, nums: List[int]) -> int:
+        
+#         n = len(nums)
+#         def calc(i):
+
+#             if i >= n-1:
+#                 return 0
+            
+#             mini = float('inf')
+#             for j in range(i + 1,min(i+nums[i]+1,n)):
+#                 mini = min(mini,1 + calc(j))
+            
+#             return mini
+        
+
+#         return calc(0)
