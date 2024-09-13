2943f005b4792d3528867e09d8dfa209e7183078
Shuaib Jawid Shahna <100420318+ShubJas@users.noreply.github.com>
2024-09-14 00:21:50 +0530

    Time: 131 ms (27.23%) | Memory: 19.1 MB (82.51%) - LeetSync

--- 135-candy/candy.py
+++ 135-candy/candy.py
@@ -5,7 +5,6 @@ class Solution:
 
 
         left = [1]
-        right = [1]
         n = len(ratings)
 
         for i in range(n-1):
@@ -14,20 +13,46 @@ class Solution:
                 left.append(left[-1] + 1)
             else:
                 left.append(1)
-            
-            if ratings[n-i-2] > ratings[n-i-1]:
-                right.append(right[-1] + 1)
+        S = 0
+        right = curr = 1
+        for i in range(n-2,-1,-1):
+            if ratings[i] > ratings[i+1]:
+                curr = right + 1
+                right = curr
             else:
-                right.append(1)
+                curr = 1
+                right = 1
+            
+            S += max(left[i],curr) 
+
+        return S + max(1,left[-1])
+
+# #  O(N) TC , 0(2N) SC
+# class Solution:
+#     def candy(self, ratings: List[int]) -> int:
+        
+
+
+#         arr = [1]
+#         n = len(ratings)
+
+#         for i in range(n-1):
+#             left = 1
+#             if ratings[i+1] > ratings[i]:
+#                 left.append(left[-1] + 1)
+#             else:
+#                 left.append(1)
+            
+#             if ratings[n-i-2] > ratings[n-i-1]:
+#                 right.append(right[-1] + 1)
+#             else:
+#                 right.append(1)
         
 
 
         
-        right = right[::-1]
+#         right = right[::-1]
 
 
-        # print([max(l,r) for l,r in zip(left,right)])
-        return sum([max(l,r) for l,r in zip(left,right)])
-
-
-
+#         # print([max(l,r) for l,r in zip(left,right)])
+#         return sum([max(l,r) for l,r in zip(left,right)])
