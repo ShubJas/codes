d6b71396eb11b84db8b0d7bff165b4cb9bcfa56f
Shuaib Jawid Shahna <100420318+ShubJas@users.noreply.github.com>
2024-09-13 15:53:07 +0530

    Time: 304 ms (54.49%) | Memory: 31.1 MB (81.84%) - LeetSync

--- 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.py
+++ 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.py
@@ -1,20 +1,39 @@
 class Solution:
     def xorQueries(self, arr: List[int], queries: List[List[int]]) -> List[int]:
-        
-        prefix_xor = []
-        xor = 0
+
+
+        n = len(arr)
         result =  []
-        for n in arr:
-            xor ^= n
-            prefix_xor.append(xor)
+        for i in range(1,n):
+            arr[i] ^= arr[i-1] 
         
         for i, j in queries:
             if i != 0:
-                result.append(prefix_xor[i-1]^prefix_xor[j])
+                result.append(arr[i-1]^arr[j])
             else:
-                result.append(prefix_xor[j])
+                result.append(arr[j])
         
         return result
+
+
+# class Solution:
+#     def xorQueries(self, arr: List[int], queries: List[List[int]]) -> List[int]:
+        
+#         prefix_xor = []
+#         xor = 0
+#         result =  []
+#         for n in arr:
+#             xor ^= n
+#             prefix_xor.append(xor)
+        
+#         for i, j in queries:
+#             if i != 0:
+#                 result.append(prefix_xor[i-1]^prefix_xor[j])
+#             else:
+#                 result.append(prefix_xor[j])
+        
+#         return result
+
 
 # Brute
 # class Solution:
