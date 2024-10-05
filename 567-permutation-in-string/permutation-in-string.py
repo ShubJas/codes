922616631c67e28789e98909817aa57d8c225bac
Shuaib Jawid Shahna <100420318+ShubJas@users.noreply.github.com>
2024-10-05 09:12:08 +0530

    Time: 57 ms (68.26%) | Memory: 16.8 MB (22.10%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,25 +1,68 @@
+#  More optimal ( no hashmap comparison)
+
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        hashmap1 = defaultdict(int)
-        hashmap2 =  defaultdict(int)
-        for c in s1:
-            hashmap1[c]+=1
+        if len(s1) > len(s2):
+            return False
         
-        winlength=len(s1)
-
-        for c in s2[:winlength]:
-            hashmap2[c]+=1
+        s1Count, s2Count = [0] * 26, [0] * 26
+        for i in range(len(s1)):
+            s1Count[ord(s1[i]) - ord('a')] += 1
+            s2Count[ord(s2[i]) - ord('a')] += 1
         
-        if hashmap1 == hashmap2:
-            return True
+        matches = 0
+        for i in range(26):
+            matches += (1 if s1Count[i] == s2Count[i] else 0)
         
-        for i in range(winlength,len(s2)):
-            hashmap2[s2[i]]+=1
-            if hashmap2[s2[i-winlength]] == 1:
-                del hashmap2[s2[i-winlength]]
-            else:
-                hashmap2[s2[i-winlength]]-=1
-            
-            if hashmap1 == hashmap2:
+        l = 0
+        for r in range(len(s1), len(s2)):
+            if matches == 26:
                 return True
-        return False
+            
+            index = ord(s2[r]) - ord('a')
+            s2Count[index] += 1
+            if s1Count[index] == s2Count[index]:
+                matches += 1
+            elif s1Count[index] + 1 == s2Count[index]:
+                matches -= 1
+            
+            index = ord(s2[l]) - ord('a')
+            s2Count[index] -= 1
+            if s1Count[index] == s2Count[index]:
+                matches += 1
+            elif s1Count[index] - 1 == s2Count[index]:
+                matches -= 1
+            
+            l += 1
+        
+        return matches == 26
+
+
+
+
+
+# class Solution:
+#     def checkInclusion(self, s1: str, s2: str) -> bool:
+#         hashmap1 = defaultdict(int)
+#         hashmap2 =  defaultdict(int)
+#         for c in s1:
+#             hashmap1[c]+=1
+        
+#         winlength=len(s1)
+
+#         for c in s2[:winlength]:
+#             hashmap2[c]+=1
+        
+#         if hashmap1 == hashmap2:
+#             return True
+        
+#         for i in range(winlength,len(s2)):
+#             hashmap2[s2[i]]+=1
+#             if hashmap2[s2[i-winlength]] == 1:
+#                 del hashmap2[s2[i-winlength]]
+#             else:
+#                 hashmap2[s2[i-winlength]]-=1
+            
+#             if hashmap1 == hashmap2:
+#                 return True
+#         return False
